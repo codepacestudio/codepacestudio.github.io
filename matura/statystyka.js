a907765bae2f271697e1cbfa882b1b70aa3dd65e
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:53:32 +0100

    Update statystyka.js

--- matura/statystyka.js
+++ matura/statystyka.js
@@ -1,1 +1,157 @@
+bazaZadan['statystyka'] = [
+    // PODDZIAŁ 1: Średnia arytmetyczna i ważona (10 zadań)
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Oblicz średnią arytmetyczną zestawu danych: 2, 4, 6, 8, 10.',
+        rozwiazanie: 'Dodajemy wszystkie liczby i dzielimy przez ich ilość (jest ich 5).<br>\\( \\frac{2 + 4 + 6 + 8 + 10}{5} = \\frac{30}{5} = 6 \\)'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Średnia arytmetyczna liczb x, 4, 5, 7 wynosi 5. Oblicz x.',
+        rozwiazanie: 'Układamy równanie. Mamy 4 liczby.<br>\\( \\frac{x + 4 + 5 + 7}{4} = 5 \\)<br>Mnożymy przez 4: \\( x + 16 = 20 \\Rightarrow x = 4 \\)'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Oblicz średnią ocen ucznia: 3, 4, 4, 5, 5, 6.',
+        rozwiazanie: 'Suma ocen to: \\( 3 + 8 + 10 + 6 = 27 \\). Ilość ocen to 6.<br>\\( \\frac{27}{6} = 4.5 \\)'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Student otrzymał ocenę 5 z wagi 3 oraz ocenę 3 z wagi 2. Oblicz średnią ważoną.',
+        rozwiazanie: 'Wzór na średnią ważoną: suma iloczynów ocen i wag przez sumę wag.<br>\\( \\frac{5 \\cdot 3 + 3 \\cdot 2}{3 + 2} = \\frac{15 + 6}{5} = \\frac{21}{5} = 4.2 \\)'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Średnia arytmetyczna 4 liczb wynosi 6. Dopisujemy do nich liczbę 11. Jaka jest nowa średnia?',
+        rozwiazanie: 'Suma początkowych 4 liczb to \\( 4 \\cdot 6 = 24 \\).<br>Nowa suma to \\( 24 + 11 = 35 \\). Mamy teraz 5 liczb.<br>Nowa średnia: \\( \\frac{35}{5} = 7 \\)'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Średnia zarobków 10 pracowników wynosi 4000 zł. Każdy dostał podwyżkę 500 zł. Ile wynosi nowa średnia?',
+        rozwiazanie: 'Jeśli do każdej danej w zestawie dodamy stałą wartość, to średnia arytmetyczna wzrośnie dokładnie o tę samą wartość.<br>Nowa średnia to 4500 zł.'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Średnia wieku grupy 5 osób wynosi 20 lat. Z grupy odeszła jedna osoba, która ma 28 lat. Oblicz średnią wieku pozostałych osób.',
+        rozwiazanie: 'Początkowa suma lat: \\( 5 \\cdot 20 = 100 \\).<br>Suma bez tej osoby: \\( 100 - 28 = 72 \\). Pozostało 4 ludzi.<br>Nowa średnia: \\( \\frac{72}{4} = 18 \\) lat.'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Z tabeli częstości: ocena 1 (2 osoby), ocena 2 (3 osoby), ocena 3 (5 osób). Oblicz średnią.',
+        rozwiazanie: 'Mnożymy ocenę przez liczbę osób: \\( 1 \\cdot 2 + 2 \\cdot 3 + 3 \\cdot 5 = 2 + 6 + 15 = 23 \\).<br>Liczba wszystkich osób to 10.<br>Średnia: \\( \\frac{23}{10} = 2.3 \\)'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'W firmie 4 osoby zarabiają 3000 zł, a prezes zarabia 18000 zł. Oblicz średnią pensję.',
+        rozwiazanie: 'Suma: \\( 4 \\cdot 3000 + 18000 = 12000 + 18000 = 30000 \\).<br>Dzielimy przez 5 osób: \\( \\frac{30000}{5} = 6000 \\) zł.'
+    },
+    {
+        poddzial: 'Średnia arytmetyczna i ważona',
+        tresc: 'Średnia z zestawu 2, x, 8 jest równa średniej z zestawu 2, 8. Oblicz x.',
+        rozwiazanie: 'Średnia z 2 i 8 to 5.<br>Zatem: \\( \\frac{2 + x + 8}{3} = 5 \\Rightarrow \\frac{10 + x}{3} = 5 \\Rightarrow 10 + x = 15 \\Rightarrow x = 5 \\).'
+    },
 
+    // PODDZIAŁ 2: Mediana i dominanta (10 zadań)
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Oblicz medianę zestawu danych: 1, 3, 5, 7, 9.',
+        rozwiazanie: 'Zbiór jest uporządkowany. Liczba elementów jest nieparzysta (5). Mediana to wartość środkowa.<br>Mediana wynosi 5.'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Oblicz medianę zestawu: 2, 4, 6, 8.',
+        rozwiazanie: 'Liczba elementów jest parzysta (4). Mediana to średnia arytmetyczna dwóch środkowych wyrazów.<br>\\( \\frac{4 + 6}{2} = 5 \\).'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Wyznacz dominantę zestawu: 1, 2, 2, 3, 4, 4, 4, 5.',
+        rozwiazanie: 'Dominanta to wartość, która występuje w zestawie najczęściej.<br>Liczba 4 występuje trzy razy. Dominanta to 4.'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Oblicz medianę danych nieuporządkowanych: 7, 2, 9, 1, 5.',
+        rozwiazanie: 'Zawsze zaczynamy od uporządkowania rosnąco: 1, 2, 5, 7, 9.<br>Element środkowy to 5. Mediana to 5.'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Znajdź x, jeśli mediana uporządkowanego zestawu 2, 4, x, 8 wynosi 5.',
+        rozwiazanie: 'Liczba elementów jest parzysta. Środkowe wyrazy to 4 i x.<br>\\( \\frac{4 + x}{2} = 5 \\Rightarrow 4 + x = 10 \\Rightarrow x = 6 \\).'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Z tabeli: Wartość 1 (ilość 10), Wartość 2 (ilość 15), Wartość 3 (ilość 5). Podaj dominantę.',
+        rozwiazanie: 'Szukamy wartości o największej liczebności (częstości).<br>Najczęściej pojawia się wartość 2 (aż 15 razy). Dominanta to 2.'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Podaj dominantę w zestawie: 1, 1, 2, 2, 3.',
+        rozwiazanie: 'Zarówno 1, jak i 2 występują najczęściej (po dwa razy).<br>Zbiór ma dwie dominanty: 1 oraz 2.'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Oblicz medianę 10 kolejnych liczb naturalnych od 1 do 10.',
+        rozwiazanie: 'Mamy 10 liczb. Środkowe to piąta i szósta, czyli 5 i 6.<br>Mediana to średnia z nich: \\( \\frac{5 + 6}{2} = 5.5 \\).'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Mediana z zestawu 1, 2, 3 wynosi 2. Dodajemy do zestawu liczbę 100. Ile wynosi nowa mediana?',
+        rozwiazanie: 'Nowy uporządkowany zestaw to 1, 2, 3, 100.<br>Środkowe wyrazy to 2 i 3. Mediana to \\( \\frac{2 + 3}{2} = 2.5 \\).'
+    },
+    {
+        poddzial: 'Mediana i dominanta',
+        tresc: 'Oblicz medianę i dominantę z ocen: 3, 3, 4, 5, 5, 5.',
+        rozwiazanie: 'Zestaw ma 6 ocen. Środkowe to 4 i 5. Mediana = 4.5.<br>Najczęściej występuje 5 (trzy razy). Dominanta = 5.'
+    },
+
+    // PODDZIAŁ 3: Wariancja i odchylenie standardowe (10 zadań)
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Oblicz wariancję zestawu danych: 2, 4, 6.',
+        rozwiazanie: '1. Średnia: \\( \\frac{2+4+6}{3} = 4 \\).<br>2. Odchylenia od średniej: (2-4)=-2, (4-4)=0, (6-4)=2.<br>3. Kwadraty odchyleń: 4, 0, 4. Ich suma to 8.<br>4. Wariancja to średnia z kwadratów odchyleń: \\( \\sigma^2 = \\frac{8}{3} \\).'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Oblicz odchylenie standardowe zestawu: 1, 3, 5.',
+        rozwiazanie: '1. Średnia = 3.<br>2. Odchylenia: -2, 0, 2.<br>3. Kwadraty: 4, 0, 4. Suma = 8.<br>4. Wariancja = \\( \\frac{8}{3} \\).<br>Odchylenie standardowe to pierwiastek z wariancji: \\( \\sigma = \\sqrt{\\frac{8}{3}} \\).'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Podaj wariancję dla zestawu składającego się z samych piątek: 5, 5, 5, 5.',
+        rozwiazanie: 'Wszystkie liczby są równe, więc średnia wynosi 5. Żadna liczba nie odchyla się od średniej (odchylenia to 0).<br>Wariancja wynosi 0.'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Oblicz wariancję dla zestawu dwóch liczb: 2 i 8.',
+        rozwiazanie: 'Średnia = 5.<br>Kwadraty odchyleń: \\( (2-5)^2 = 9 \\), \\( (8-5)^2 = 9 \\).<br>Wariancja: \\( \\frac{9 + 9}{2} = 9 \\).'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Oblicz odchylenie standardowe dla zestawu 2 i 8.',
+        rozwiazanie: 'W poprzednim zadaniu policzyliśmy, że wariancja dla tego zestawu wynosi 9.<br>Odchylenie to pierwiastek z wariancji: \\( \\sigma = \\sqrt{9} = 3 \\).'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Wiadomo, że wariancja pewnego zestawu danych wynosi 16. Ile wynosi odchylenie standardowe?',
+        rozwiazanie: 'Odchylenie standardowe to zawsze pierwiastek kwadratowy z wariancji.<br>\\( \\sigma = \\sqrt{16} = 4 \\).'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Każdą liczbę w zestawie pomnożono przez 2. Jak zmieni się wariancja?',
+        rozwiazanie: 'Zgodnie z własnościami, jeśli wszystkie dane pomnożymy przez k, to wariancja wzrasta o k do kwadratu.<br>Wariancja powiększy się 4 razy.'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Każdą liczbę w zestawie danych zwiększono o 5. Jak zmieni się odchylenie standardowe?',
+        rozwiazanie: 'Dodanie stałej wartości do każdej liczby w zestawie przesuwa cały zbiór, ale nie zmienia rozrzutu danych.<br>Odchylenie standardowe nie zmieni się.'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Oblicz wariancję dla danych: -1, 0, 1.',
+        rozwiazanie: 'Średnia = 0.<br>Kwadraty odchyleń: \\( (-1-0)^2 = 1 \\), \\( 0^2 = 0 \\), \\( (1-0)^2 = 1 \\).<br>Suma = 2. Liczba elementów = 3.<br>Wariancja = \\( \\frac{2}{3} \\).'
+    },
+    {
+        poddzial: 'Wariancja i odchylenie',
+        tresc: 'Średnia arytmetyczna pięciu liczb to 10. Ich wariancja to 0. Wypisz te liczby.',
+        rozwiazanie: 'Wariancja równa 0 oznacza brak jakiegokolwiek odchylenia od średniej. Wszystkie liczby muszą być identyczne i równe średniej.<br>Te liczby to: 10, 10, 10, 10, 10.'
+    }
+];
