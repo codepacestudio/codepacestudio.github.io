e3ed82d442cc8111bb6f455e070874f5e3a8f7ac
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:51:30 +0100

    Update stereometria.js

--- matura/stereometria.js
+++ matura/stereometria.js
@@ -1,1 +1,157 @@
+bazaZadan['stereometria'] = [
+    // PODDZIAŁ 1: Graniastosłupy (10 zadań)
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Oblicz objętość sześcianu, którego krawędź ma długość 3.',
+        rozwiazanie: 'Wzór na objętość sześcianu to \\( V = a^3 \\).<br>\\( V = 3^3 = 27 \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Pole powierzchni całkowitej sześcianu wynosi 96. Oblicz długość jego krawędzi.',
+        rozwiazanie: 'Sześcian ma 6 identycznych kwadratowych ścian. \\( P_c = 6a^2 \\).<br>\\( 6a^2 = 96 \\Rightarrow a^2 = 16 \\Rightarrow a = 4 \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Oblicz długość przekątnej sześcianu o krawędzi 5.',
+        rozwiazanie: 'Wzór na przekątną sześcianu to \\( d = a\\sqrt{3} \\).<br>\\( d = 5\\sqrt{3} \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Prostopadłościan ma wymiary 2, 3 i 4. Oblicz jego objętość.',
+        rozwiazanie: 'Objętość prostopadłościanu to iloczyn jego trzech wymiarów: \\( V = a \\cdot b \\cdot c \\).<br>\\( V = 2 \\cdot 3 \\cdot 4 = 24 \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Oblicz pole powierzchni całkowitej prostopadłościanu o wymiarach 1, 2 i 3.',
+        rozwiazanie: 'Liczymy pola par ścian: dwie o wymiarach 1x2, dwie 1x3 i dwie 2x3.<br>\\( P_c = 2(1 \\cdot 2 + 1 \\cdot 3 + 2 \\cdot 3) = 2(2 + 3 + 6) = 2 \\cdot 11 = 22 \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Krawędzie prostopadłościanu mają długości 3, 4 i 12. Oblicz długość jego przekątnej.',
+        rozwiazanie: 'Wzór na przekątną prostopadłościanu: \\( d = \\sqrt{a^2 + b^2 + c^2} \\).<br>\\( d = \\sqrt{3^2 + 4^2 + 12^2} = \\sqrt{9 + 16 + 144} = \\sqrt{169} = 13 \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Graniastosłup prawidłowy trójkątny ma krawędź podstawy 4 i wysokość 10. Oblicz jego objętość.',
+        rozwiazanie: 'Podstawą jest trójkąt równoboczny. \\( P_p = \\frac{a^2\\sqrt{3}}{4} = \\frac{16\\sqrt{3}}{4} = 4\\sqrt{3} \\).<br>Objętość \\( V = P_p \\cdot h = 4\\sqrt{3} \\cdot 10 = 40\\sqrt{3} \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Oblicz pole powierzchni bocznej graniastosłupa prawidłowego czworokątnego o krawędzi podstawy 2 i wysokości 5.',
+        rozwiazanie: 'Powierzchnia boczna to 4 identyczne prostokąty o wymiarach 2 na 5.<br>\\( P_b = 4 \\cdot (2 \\cdot 5) = 4 \\cdot 10 = 40 \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Objętość graniastosłupa wynosi 100, a jego wysokość 20. Oblicz pole jego podstawy.',
+        rozwiazanie: 'Wzór ogólny: \\( V = P_p \\cdot h \\).<br>\\( 100 = P_p \\cdot 20 \\Rightarrow P_p = 5 \\)'
+    },
+    {
+        poddzial: 'Graniastosłupy',
+        tresc: 'Suma długości wszystkich krawędzi sześcianu wynosi 48. Oblicz jego objętość.',
+        rozwiazanie: 'Sześcian ma 12 równych krawędzi. \\( 12a = 48 \\Rightarrow a = 4 \\).<br>Objętość: \\( V = 4^3 = 64 \\)'
+    },
 
+    // PODDZIAŁ 2: Ostrosłupy (10 zadań)
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Oblicz objętość ostrosłupa o polu podstawy 15 i wysokości 10.',
+        rozwiazanie: 'Wzór na objętość ostrosłupa: \\( V = \\frac{1}{3} \\cdot P_p \\cdot h \\).<br>\\( V = \\frac{1}{3} \\cdot 15 \\cdot 10 = 5 \\cdot 10 = 50 \\)'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Ostrosłup prawidłowy czworokątny ma krawędź podstawy 6 i wysokość 10. Oblicz objętość.',
+        rozwiazanie: 'Podstawą jest kwadrat. \\( P_p = 6^2 = 36 \\).<br>\\( V = \\frac{1}{3} \\cdot 36 \\cdot 10 = 12 \\cdot 10 = 120 \\)'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Czworościan foremny ma krawędź o długości 2. Oblicz jego pole powierzchni całkowitej.',
+        rozwiazanie: 'Czworościan foremny składa się z 4 trójkątów równobocznych.<br>\\( P_c = 4 \\cdot \\frac{a^2\\sqrt{3}}{4} = a^2\\sqrt{3} \\).<br>\\( P_c = 2^2\\sqrt{3} = 4\\sqrt{3} \\)'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'W ostrosłupie prawidłowym czworokątnym wysokość wynosi 4, a krawędź podstawy 6. Oblicz wysokość ściany bocznej.',
+        rozwiazanie: 'Tworzymy trójkąt prostokątny z wysokości ostrosłupa (4), połowy krawędzi podstawy (3) i wysokości ściany bocznej (h_b).<br>Z Pitagorasa: \\( h_b^2 = 4^2 + 3^2 = 16 + 9 = 25 \\Rightarrow h_b = 5 \\).'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Pole podstawy ostrosłupa wynosi 20, a jego objętość to 60. Oblicz wysokość.',
+        rozwiazanie: 'Korzystamy ze wzoru: \\( V = \\frac{1}{3} \\cdot P_p \\cdot H \\).<br>\\( 60 = \\frac{1}{3} \\cdot 20 \\cdot H \\)<br>\\( 180 = 20H \\Rightarrow H = 9 \\)'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Ostrosłup prawidłowy trójkątny ma krawędź podstawy 2 i wysokość 3. Oblicz objętość.',
+        rozwiazanie: 'Pole podstawy (trójkąt równoboczny): \\( P_p = \\frac{2^2\\sqrt{3}}{4} = \\sqrt{3} \\).<br>\\( V = \\frac{1}{3} \\cdot \\sqrt{3} \\cdot 3 = \\sqrt{3} \\)'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Podstawą ostrosłupa jest prostokąt o bokach 3 i 4. Wysokość ostrosłupa to 5. Oblicz objętość.',
+        rozwiazanie: 'Pole prostokąta w podstawie: \\( P_p = 3 \\cdot 4 = 12 \\).<br>\\( V = \\frac{1}{3} \\cdot 12 \\cdot 5 = 4 \\cdot 5 = 20 \\)'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'W ostrosłupie prawidłowym czworokątnym krawędź boczna ma długość 5, a przekątna podstawy \\( 6\\sqrt{2} \\). Oblicz wysokość ostrosłupa.',
+        rozwiazanie: 'Wysokość ostrosłupa (H), połowa przekątnej podstawy (d/2) i krawędź boczna (k) tworzą trójkąt prostokątny.<br>\\( \\frac{d}{2} = 3\\sqrt{2} \\).<br>Z Pitagorasa: \\( H^2 + (3\\sqrt{2})^2 = 5^2 \\Rightarrow H^2 + 18 = 25 \\Rightarrow H^2 = 7 \\Rightarrow H = \\sqrt{7} \\).'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Pole powierzchni całkowitej ostrosłupa wynosi 50. Pole jego podstawy to 20. Oblicz pole powierzchni bocznej.',
+        rozwiazanie: 'Pole całkowite to suma pola podstawy i pola bocznego: \\( P_c = P_p + P_b \\).<br>\\( 50 = 20 + P_b \\Rightarrow P_b = 30 \\)'
+    },
+    {
+        poddzial: 'Ostrosłupy',
+        tresc: 'Kąt nachylenia krawędzi bocznej do podstawy w ostrosłupie prawidłowym czworokątnym wynosi 45 stopni. Wysokość ostrosłupa to 4. Oblicz przekątną podstawy.',
+        rozwiazanie: 'Trójkąt utworzony przez wysokość ostrosłupa, połowę przekątnej podstawy i krawędź boczną to trójkąt prostokątny równoramienny (kąt 45 st.).<br>Zatem połowa przekątnej jest równa wysokości, czyli wynosi 4. Cała przekątna to 8.'
+    },
+
+    // PODDZIAŁ 3: Bryły obrotowe (10 zadań)
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Oblicz objętość walca o promieniu podstawy 3 i wysokości 5.',
+        rozwiazanie: 'Wzór na objętość walca: \\( V = \\pi r^2 h \\).<br>\\( V = \\pi \\cdot 3^2 \\cdot 5 = 9\\pi \\cdot 5 = 45\\pi \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Pole powierzchni bocznej walca wynosi \\( 16\\pi \\). Promień podstawy to 2. Oblicz wysokość.',
+        rozwiazanie: 'Wzór na pole boczne walca: \\( P_b = 2\\pi r h \\).<br>\\( 16\\pi = 2\\pi \\cdot 2 \\cdot h \\)<br>\\( 16\\pi = 4\\pi h \\Rightarrow h = 4 \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Oblicz objętość stożka, którego promień podstawy to 4, a wysokość 6.',
+        rozwiazanie: 'Wzór na objętość stożka: \\( V = \\frac{1}{3} \\pi r^2 h \\).<br>\\( V = \\frac{1}{3} \\pi \\cdot 4^2 \\cdot 6 = \\frac{1}{3} \\pi \\cdot 16 \\cdot 6 = 32\\pi \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Oblicz pole powierzchni bocznej stożka, w którym promień wynosi 3, a tworząca 5.',
+        rozwiazanie: 'Wzór na pole boczne stożka: \\( P_b = \\pi r l \\).<br>\\( P_b = \\pi \\cdot 3 \\cdot 5 = 15\\pi \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'W stożku promień podstawy to 3, a wysokość to 4. Oblicz długość tworzącej stożka.',
+        rozwiazanie: 'W stożku promień r, wysokość h i tworząca l tworzą trójkąt prostokątny.<br>Z Pitagorasa: \\( l^2 = r^2 + h^2 = 3^2 + 4^2 = 9 + 16 = 25 \\Rightarrow l = 5 \\).'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Oblicz objętość kuli o promieniu 3.',
+        rozwiazanie: 'Wzór na objętość kuli: \\( V = \\frac{4}{3} \\pi R^3 \\).<br>\\( V = \\frac{4}{3} \\pi \\cdot 3^3 = \\frac{4}{3} \\pi \\cdot 27 = 36\\pi \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Oblicz pole powierzchni kuli o promieniu 4.',
+        rozwiazanie: 'Wzór na pole sfery: \\( P = 4\\pi R^2 \\).<br>\\( P = 4\\pi \\cdot 4^2 = 4\\pi \\cdot 16 = 64\\pi \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Przekrój osiowy walca jest kwadratem o boku 6. Oblicz objętość tego walca.',
+        rozwiazanie: 'Skoro przekrój to kwadrat o boku 6, to wysokość walca h = 6, a średnica podstawy (2r) też wynosi 6, więc r = 3.<br>\\( V = \\pi r^2 h = \\pi \\cdot 3^2 \\cdot 6 = \\pi \\cdot 9 \\cdot 6 = 54\\pi \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Przekrój osiowy stożka jest trójkątem równobocznym o boku 4. Oblicz pole powierzchni bocznej tego stożka.',
+        rozwiazanie: 'Z treści zadania wynika, że tworząca l = 4, a średnica to 4, więc promień r = 2.<br>\\( P_b = \\pi r l = \\pi \\cdot 2 \\cdot 4 = 8\\pi \\)'
+    },
+    {
+        poddzial: 'Bryły obrotowe',
+        tresc: 'Pole powierzchni kuli wynosi \\( 100\\pi \\). Oblicz promień tej kuli.',
+        rozwiazanie: 'Korzystamy ze wzoru na pole sfery: \\( 4\\pi R^2 = 100\\pi \\).<br>Dzielimy przez \\( 4\\pi \\): \\( R^2 = 25 \\Rightarrow R = 5 \\).'
+    }
+];
