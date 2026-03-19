29af60edda916b3afa76a3e15828933434f55787
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:50:48 +0100

    Update analityczna.js

--- matura/analityczna.js
+++ matura/analityczna.js
@@ -1,1 +1,157 @@
+bazaZadan['analityczna'] = [
+    // PODDZIAŁ 1: Równanie prostej (10 zadań)
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Znajdź punkt przecięcia prostej o równaniu \\( y = -3x + 6 \\) z osią OY.',
+        rozwiazanie: 'Punkt przecięcia z osią OY ma współrzędną x równą 0.<br>Podstawiamy do wzoru: \\( y = -3 \\cdot 0 + 6 = 6 \\).<br>Szukany punkt to (0, 6).'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Wyznacz miejsce zerowe funkcji liniowej o równaniu \\( y = 2x - 8 \\).',
+        rozwiazanie: 'Miejsce zerowe to punkt przecięcia z osią OX, czyli y = 0.<br>\\( 0 = 2x - 8 \\)<br>\\( 2x = 8 \\Rightarrow x = 4 \\).'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Sprawdź rachunkowo, czy punkt A(-2, 5) należy do prostej \\( y = -x + 3 \\).',
+        rozwiazanie: 'Podstawiamy współrzędne punktu do równania prostej (x = -2, y = 5):<br>\\( 5 = -(-2) + 3 \\)<br>\\( 5 = 2 + 3 \\)<br>\\( 5 = 5 \\). Punkt należy do prostej.'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Napisz równanie prostej przechodzącej przez punkty A(0, 4) i B(2, 10).',
+        rozwiazanie: 'Wzór prostej to \\( y = ax + b \\). Z punktu A od razu wiemy, że wyraz wolny \\( b = 4 \\).<br>Podstawiamy punkt B: \\( 10 = a \\cdot 2 + 4 \\)<br>\\( 2a = 6 \\Rightarrow a = 3 \\).<br>Równanie to \\( y = 3x + 4 \\).'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Podaj współczynnik kierunkowy prostej o równaniu ogólnym \\( 2x - y + 5 = 0 \\).',
+        rozwiazanie: 'Przekształcamy równanie do postaci kierunkowej \\( y = ax + b \\):<br>\\( -y = -2x - 5 \\)<br>\\( y = 2x + 5 \\).<br>Współczynnik kierunkowy a wynosi 2.'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Dla jakiego parametru m punkt P(1, m) należy do prostej \\( y = 4x - 2 \\)?',
+        rozwiazanie: 'Podstawiamy x = 1 oraz y = m do równania:<br>\\( m = 4 \\cdot 1 - 2 \\)<br>\\( m = 4 - 2 = 2 \\).'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Prosta przechodzi przez początek układu współrzędnych i punkt A(3, 9). Podaj jej równanie.',
+        rozwiazanie: 'Przechodzi przez (0,0), więc wyraz wolny \\( b = 0 \\). Równanie ma postać \\( y = ax \\).<br>Podstawiamy A(3, 9): \\( 9 = a \\cdot 3 \\Rightarrow a = 3 \\).<br>Równanie: \\( y = 3x \\).'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Oblicz współczynnik kierunkowy prostej przechodzącej przez punkty C(-1, 2) i D(3, 10).',
+        rozwiazanie: 'Korzystamy ze wzoru \\( a = \\frac{y_2 - y_1}{x_2 - x_1} \\).<br>\\( a = \\frac{10 - 2}{3 - (-1)} = \\frac{8}{4} = 2 \\).'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Prosta tworzy z osią OX kąt 45 stopni i przecina oś OY w punkcie (0, -2). Podaj jej równanie.',
+        rozwiazanie: 'Współczynnik kierunkowy to tangens kąta nachylenia: \\( a = \\tg 45^\\circ = 1 \\).<br>Wyraz wolny b odczytujemy z przecięcia z OY: \\( b = -2 \\).<br>Równanie: \\( y = 1x - 2 \\), czyli \\( y = x - 2 \\).'
+    },
+    {
+        poddzial: 'Równanie prostej',
+        tresc: 'Przekształć równanie kierunkowe \\( y = -\\frac{1}{2}x + 3 \\) do postaci ogólnej.',
+        rozwiazanie: 'Postać ogólna to \\( Ax + By + C = 0 \\).<br>Mnożymy wszystko przez 2, aby pozbyć się ułamka: \\( 2y = -x + 6 \\).<br>Przenosimy wszystko na jedną stronę: \\( x + 2y - 6 = 0 \\).'
+    },
 
+    // PODDZIAŁ 2: Proste równoległe i prostopadłe (10 zadań)
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Napisz równanie prostej równoległej do \\( y = 4x - 7 \\) przechodzącej przez punkt P(2, 5).',
+        rozwiazanie: 'Proste równoległe mają ten sam współczynnik a. Zatem nasza prosta to \\( y = 4x + b \\).<br>Podstawiamy P(2, 5): \\( 5 = 4 \\cdot 2 + b \\Rightarrow 5 = 8 + b \\Rightarrow b = -3 \\).<br>Odpowiedź: \\( y = 4x - 3 \\).'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Podaj równanie prostej prostopadłej do \\( y = 2x + 1 \\) przechodzącej przez punkt A(4, 0).',
+        rozwiazanie: 'Współczynnik a dla prostej prostopadłej to odwrotność z minusem: \\( a_2 = -\\frac{1}{2} \\). Równanie: \\( y = -\\frac{1}{2}x + b \\).<br>Podstawiamy A(4, 0): \\( 0 = -\\frac{1}{2} \\cdot 4 + b \\Rightarrow 0 = -2 + b \\Rightarrow b = 2 \\).<br>Odpowiedź: \\( y = -\\frac{1}{2}x + 2 \\).'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Dla jakiego parametru m proste \\( y = (m-1)x + 3 \\) oraz \\( y = 5x - 2 \\) są równoległe?',
+        rozwiazanie: 'Warunek równoległości to równe współczynniki kierunkowe.<br>\\( m - 1 = 5 \\Rightarrow m = 6 \\).'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Dla jakiego m proste \\( y = 2x + 4 \\) i \\( y = mx - 1 \\) są prostopadłe?',
+        rozwiazanie: 'Iloczyn współczynników kierunkowych prostych prostopadłych musi wynosić -1 (czyli \\( a_1 \\cdot a_2 = -1 \\)).<br>\\( 2 \\cdot m = -1 \\Rightarrow m = -\\frac{1}{2} \\).'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Określ wzajemne położenie prostych: \\( y = 3x - 1 \\) oraz \\( 6x - 2y + 4 = 0 \\).',
+        rozwiazanie: 'Przekształcamy drugą prostą do postaci kierunkowej:<br>\\( -2y = -6x - 4 \\) (dzielimy przez -2)<br>\\( y = 3x + 2 \\).<br>Mają równe współczynniki a (równe 3) i różne b. Proste są równoległe.'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Napisz równanie prostej prostopadłej do osi OX, przechodzącej przez punkt (-3, 7).',
+        rozwiazanie: 'Prosta prostopadła do osi OX to prosta pionowa. Jej równanie ma zawsze postać \\( x = c \\).<br>Skoro przechodzi przez punkt o współrzędnej x = -3, to równanie brzmi: \\( x = -3 \\).'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Napisz równanie prostej równoległej do osi OX, przechodzącej przez punkt (4, -2).',
+        rozwiazanie: 'Prosta równoległa do osi OX to prosta pozioma (funkcja stała). Jej równanie to \\( y = c \\).<br>Skoro przechodzi przez punkt o współrzędnej y = -2, to równanie brzmi: \\( y = -2 \\).'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Dla jakiej wartości m proste \\( y = -\\frac{1}{3}x + 2 \\) oraz \\( y = (2m+1)x \\) są prostopadłe?',
+        rozwiazanie: 'Współczynnik drugiej prostej musi być odwrotnością pierwszego ze zmienionym znakiem. Odwrotność z minusem dla \\( -\\frac{1}{3} \\) to 3.<br>Zatem: \\( 2m + 1 = 3 \\Rightarrow 2m = 2 \\Rightarrow m = 1 \\).'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Czy proste o równaniach \\( 3x - 4y + 2 = 0 \\) i \\( 4x + 3y - 1 = 0 \\) są prostopadłe?',
+        rozwiazanie: 'Wyznaczamy współczynniki a z obu równań.<br>Z pierwszej: \\( 4y = 3x + 2 \\Rightarrow y = \\frac{3}{4}x + 0.5 \\).<br>Z drugiej: \\( 3y = -4x + 1 \\Rightarrow y = -\\frac{4}{3}x + \\frac{1}{3} \\).<br>Iloczyn \\( \\frac{3}{4} \\cdot (-\\frac{4}{3}) = -1 \\). Tak, są prostopadłe.'
+    },
+    {
+        poddzial: 'Proste równoległe i prostopadłe',
+        tresc: 'Znajdź odległość między prostymi równoległymi \\( y = 2x + 1 \\) i \\( y = 2x - 4 \\). Wykorzystaj fakt, że przecinają oś OY.',
+        rozwiazanie: 'Wyznaczamy dowolny punkt na pierwszej prostej, np. dla x=0 mamy y=1, czyli P(0, 1).<br>Równanie ogólne drugiej prostej: \\( 2x - y - 4 = 0 \\).<br>Liczymy odległość punktu P od drugiej prostej ze wzoru: \\( d = \\frac{|Ax_0 + By_0 + C|}{\\sqrt{A^2 + B^2}} \\).<br>\\( d = \\frac{|2 \\cdot 0 - 1 \\cdot 1 - 4|}{\\sqrt{2^2 + (-1)^2}} = \\frac{|-5|}{\\sqrt{5}} = \\frac{5}{\\sqrt{5}} = \\sqrt{5} \\).'
+    },
+
+    // PODDZIAŁ 3: Środek odcinka i odległość punktów (10 zadań)
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Wyznacz współrzędne środka odcinka AB, jeśli A(-2, 4) i B(6, 8).',
+        rozwiazanie: 'Korzystamy ze wzoru na środek odcinka: \\( S = (\\frac{x_1+x_2}{2}, \\frac{y_1+y_2}{2}) \\).<br>Współrzędna x: \\( \\frac{-2 + 6}{2} = 2 \\).<br>Współrzędna y: \\( \\frac{4 + 8}{2} = 6 \\).<br>Środek to punkt S(2, 6).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Oblicz długość odcinka AB, gdzie A(1, 2) i B(4, 6).',
+        rozwiazanie: 'Korzystamy ze wzoru na odległość punktów: \\( |AB| = \\sqrt{(x_2 - x_1)^2 + (y_2 - y_1)^2} \\).<br>\\( |AB| = \\sqrt{(4 - 1)^2 + (6 - 2)^2} = \\sqrt{3^2 + 4^2} = \\sqrt{9 + 16} = \\sqrt{25} = 5 \\).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Punkt S(3, 5) jest środkiem odcinka AB. Znamy A(1, 1). Znajdź współrzędne punktu B.',
+        rozwiazanie: 'Oznaczamy B jako \\( (x, y) \\). Ze wzoru na środek:<br>\\( \\frac{1 + x}{2} = 3 \\Rightarrow 1 + x = 6 \\Rightarrow x = 5 \\).<br>\\( \\frac{1 + y}{2} = 5 \\Rightarrow 1 + y = 10 \\Rightarrow y = 9 \\).<br>Punkt B ma współrzędne (5, 9).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Oblicz promień okręgu o środku S(0, 0), który przechodzi przez punkt P(3, -4).',
+        rozwiazanie: 'Promień to długość odcinka SP.<br>\\( r = \\sqrt{(3 - 0)^2 + (-4 - 0)^2} = \\sqrt{9 + 16} = \\sqrt{25} = 5 \\).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Trójkąt ma wierzchołki A(-1, -1), B(5, -1), C(2, 3). Oblicz obwód tego trójkąta.',
+        rozwiazanie: 'Liczymy długości boków:<br>\\( |AB| = \\sqrt{6^2 + 0^2} = 6 \\).<br>\\( |BC| = \\sqrt{(-3)^2 + 4^2} = \\sqrt{25} = 5 \\).<br>\\( |AC| = \\sqrt{3^2 + 4^2} = \\sqrt{25} = 5 \\).<br>Obwód: \\( 6 + 5 + 5 = 16 \\).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Znajdź punkt symetryczny do punktu P(2, -3) względem początku układu współrzędnych (0,0).',
+        rozwiazanie: 'Symetria względem punktu (0,0) zmienia znaki obu współrzędnych na przeciwne.<br>Szukany punkt to P\'(-2, 3).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Oblicz długość środkowej poprowadzonej z wierzchołka C w trójkącie A(-2, 0), B(4, 0), C(1, 4).',
+        rozwiazanie: 'Środkowa łączy wierzchołek C ze środkiem boku AB. Liczymy środek AB (oznaczmy go D):<br>\\( D = (\\frac{-2+4}{2}, \\frac{0+0}{2}) = (1, 0) \\).<br>Teraz długość odcinka CD:<br>\\( |CD| = \\sqrt{(1-1)^2 + (4-0)^2} = \\sqrt{0 + 16} = 4 \\).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Punkty A(-2, 1) i C(4, 5) to przeciwległe wierzchołki prostokąta ABCD. Podaj współrzędne punktu przecięcia jego przekątnych.',
+        rozwiazanie: 'Punkt przecięcia przekątnych w prostokącie to dokładnie środek każdej z nich (np. odcinka AC).<br>\\( S = (\\frac{-2+4}{2}, \\frac{1+5}{2}) = (1, 3) \\).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Punkt A znajduje się na osi OX, a punkt B na osi OY. Środkiem odcinka AB jest S(4, -2). Znajdź współrzędne A i B.',
+        rozwiazanie: 'Skoro A leży na OX, to \\( A(x, 0) \\). Skoro B na OY, to \\( B(0, y) \\).<br>Podstawiamy do wzoru na środek: \\( \\frac{x+0}{2} = 4 \\Rightarrow x = 8 \\), więc A(8, 0).<br>Dla y: \\( \\frac{0+y}{2} = -2 \\Rightarrow y = -4 \\), więc B(0, -4).'
+    },
+    {
+        poddzial: 'Środek odcinka i odległość punktów',
+        tresc: 'Sprawdź, czy trójkąt o wierzchołkach A(0, 0), B(3, 4) i C(-4, 3) jest równoramienny.',
+        rozwiazanie: 'Liczymy długości boków:<br>\\( |AB| = \\sqrt{3^2 + 4^2} = \\sqrt{25} = 5 \\).<br>\\( |AC| = \\sqrt{(-4)^2 + 3^2} = \\sqrt{16 + 9} = 5 \\).<br>Skoro dwa boki mają tę samą długość (5), to trójkąt jest równoramienny.'
+    }
+];
