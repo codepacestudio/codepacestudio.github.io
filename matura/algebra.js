ab61df87c10d39322cdfe80c328834e4fd8e4ed4
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:41:16 +0100

    Update algebra.js

--- matura/algebra.js
+++ matura/algebra.js
@@ -1,1 +1,152 @@
-
+bazaZadan['algebra'] = [
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Zastosuj wzór skróconego mnożenia dla wyrażenia: \\( (x + 3)^2 \\)',
+        rozwiazanie: 'Korzystamy ze wzoru na kwadrat sumy: \\( (a+b)^2 = a^2 + 2ab + b^2 \\)<br>\\( (x)^2 + 2 \\cdot x \\cdot 3 + 3^2 = x^2 + 6x + 9 \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Przekształć wyrażenie: \\( (2x - 5)^2 \\)',
+        rozwiazanie: 'Korzystamy ze wzoru na kwadrat różnicy: \\( (a-b)^2 = a^2 - 2ab + b^2 \\)<br>\\( (2x)^2 - 2 \\cdot 2x \\cdot 5 + 5^2 = 4x^2 - 20x + 25 \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Oblicz korzystając ze wzorów skróconego mnożenia: \\( (x - 4)(x + 4) \\)',
+        rozwiazanie: 'Korzystamy ze wzoru na różnicę kwadratów: \\( (a-b)(a+b) = a^2 - b^2 \\)<br>\\( x^2 - 4^2 = x^2 - 16 \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Zapisz w postaci sumy algebraicznej: \\( (3x + 2y)^2 \\)',
+        rozwiazanie: 'Stosujemy kwadrat sumy:<br>\\( (3x)^2 + 2 \\cdot 3x \\cdot 2y + (2y)^2 = 9x^2 + 12xy + 4y^2 \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Rozłóż na czynniki wyrażenie: \\( x^2 - 25 \\)',
+        rozwiazanie: 'Używamy wzoru na różnicę kwadratów w odwrotną stronę:<br>\\( x^2 - 5^2 = (x - 5)(x + 5) \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Zastosuj wzór skróconego mnożenia: \\( (\\sqrt{2} - 1)^2 \\)',
+        rozwiazanie: 'Podstawiamy do wzoru na kwadrat różnicy:<br>\\( (\\sqrt{2})^2 - 2 \\cdot \\sqrt{2} \\cdot 1 + 1^2 = 2 - 2\\sqrt{2} + 1 = 3 - 2\\sqrt{2} \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Zwiń do wzoru skróconego mnożenia: \\( x^2 + 10x + 25 \\)',
+        rozwiazanie: 'Zauważamy, że to rozwinięcie kwadratu sumy:<br>\\( x^2 + 2 \\cdot x \\cdot 5 + 5^2 = (x + 5)^2 \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Przekształć: \\( (\\frac{1}{2}x - 4)^2 \\)',
+        rozwiazanie: 'Kwadrat różnicy:<br>\\( (\\frac{1}{2}x)^2 - 2 \\cdot \\frac{1}{2}x \\cdot 4 + 4^2 = \\frac{1}{4}x^2 - 4x + 16 \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Rozłóż wyrażenie na czynniki: \\( 9x^2 - 1 \\)',
+        rozwiazanie: 'Różnica kwadratów:<br>\\( (3x)^2 - 1^2 = (3x - 1)(3x + 1) \\)'
+    },
+    {
+        poddzial: 'Wzory skróconego mnożenia',
+        tresc: 'Wykonaj potęgowanie: \\( (x^2 + 3)^2 \\)',
+        rozwiazanie: 'Kwadrat sumy:<br>\\( (x^2)^2 + 2 \\cdot x^2 \\cdot 3 + 3^2 = x^4 + 6x^2 + 9 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Wykonaj redukcję wyrazów podobnych: \\( 3x^2 - 5x + 2x^2 + 8x - 4 \\)',
+        rozwiazanie: 'Grupujemy wyrazy z tymi samymi potęgami:<br>\\( (3x^2 + 2x^2) + (-5x + 8x) - 4 = 5x^2 + 3x - 4 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Wymnóż i uprość: \\( 2x(x - 3) - 3(x^2 + 2) \\)',
+        rozwiazanie: 'Mnożymy nawiasy i pamiętamy o znakach:<br>\\( 2x^2 - 6x - 3x^2 - 6 \\)<br>Redukujemy: \\( -x^2 - 6x - 6 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Wyłącz wspólny czynnik przed nawias z wyrażenia: \\( 6x^3 - 9x^2 \\)',
+        rozwiazanie: 'Największy wspólny dzielnik dla 6 i 9 to 3. Wyłączamy też najniższą potęgę x, czyli \\( x^2 \\):<br>\\( 3x^2(2x - 3) \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Wymnóż sumy algebraiczne: \\( (x - 2)(2x + 1) \\)',
+        rozwiazanie: 'Każdy element z pierwszego nawiasu mnożymy przez każdy z drugiego:<br>\\( x \\cdot 2x + x \\cdot 1 - 2 \\cdot 2x - 2 \\cdot 1 = 2x^2 + x - 4x - 2 = 2x^2 - 3x - 2 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Uprość wyrażenie: \\( (x + 2)^2 - x(x + 4) \\)',
+        rozwiazanie: 'Rozwijamy wzór i mnożymy nawias:<br>\\( (x^2 + 4x + 4) - (x^2 + 4x) \\)<br>Opuszczamy nawiasy: \\( x^2 + 4x + 4 - x^2 - 4x = 4 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Sprowadź do najprostszej postaci: \\( \\frac{4x^2 - 8x}{4x} \\) dla \\( x \\neq 0 \\)',
+        rozwiazanie: 'Wyłączamy \\( 4x \\) przed nawias w liczniku:<br>\\( \\frac{4x(x - 2)}{4x} \\)<br>Skracamy przez \\( 4x \\). Otrzymujemy: \\( x - 2 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Uprość wyrażenie: \\( 5a - [2a - (3a - 1)] \\)',
+        rozwiazanie: 'Zaczynamy od wewnętrznego nawiasu, zmieniając znaki:<br>\\( 5a - [2a - 3a + 1] = 5a - [-a + 1] \\)<br>Zmieniamy znak przy opuszczaniu nawiasu kwadratowego:<br>\\( 5a + a - 1 = 6a - 1 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Wyłącz wspólny czynnik: \\( x^2y + xy^2 \\)',
+        rozwiazanie: 'Wspólnym czynnikiem dla obu wyrazów jest \\( xy \\).<br>Po wyłączeniu przed nawias: \\( xy(x + y) \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Wykonaj dzielenie wielomianów (uprość): \\( (2x^3 + 6x^2) : 2x^2 \\)',
+        rozwiazanie: 'Dzielimy każdy element przez \\( 2x^2 \\):<br>\\( \\frac{2x^3}{2x^2} + \\frac{6x^2}{2x^2} = x + 3 \\)'
+    },
+    {
+        poddzial: 'Przekształcanie wyrażeń',
+        tresc: 'Przekształć sumę: \\( \\frac{x}{2} + \\frac{x}{3} \\)',
+        rozwiazanie: 'Sprowadzamy do wspólnego mianownika (6):<br>\\( \\frac{3x}{6} + \\frac{2x}{6} = \\frac{5x}{6} \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz wartość wyrażenia \\( 2x^2 - 3x + 1 \\) dla \\( x = -2 \\)',
+        rozwiazanie: 'Podstawiamy -2 w miejsce x (pamiętając o nawiasach dla liczb ujemnych):<br>\\( 2(-2)^2 - 3(-2) + 1 = 2(4) + 6 + 1 = 8 + 6 + 1 = 15 \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz wartość wyrażenia \\( \\frac{x - 3}{x + 1} \\) dla \\( x = 5 \\)',
+        rozwiazanie: 'Podstawiamy 5 w miejsce x:<br>\\( \\frac{5 - 3}{5 + 1} = \\frac{2}{6} = \\frac{1}{3} \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz wartość wyrażenia \\( a^2 - b^2 \\) dla \\( a = 4 \\) i \\( b = 3 \\)',
+        rozwiazanie: 'Podstawiamy odpowiednie wartości:<br>\\( 4^2 - 3^2 = 16 - 9 = 7 \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Dana jest wartość \\( x = \\sqrt{2} \\). Oblicz \\( x^2 + 2x \\)',
+        rozwiazanie: 'Podstawiamy:<br>\\( (\\sqrt{2})^2 + 2(\\sqrt{2}) = 2 + 2\\sqrt{2} \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz wartość wyrażenia \\( |x - 5| \\) dla \\( x = 2 \\)',
+        rozwiazanie: 'Podstawiamy pod wartość bezwzględną:<br>\\( |2 - 5| = |-3| = 3 \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz \\( 3x^3 - x \\) dla \\( x = -1 \\)',
+        rozwiazanie: 'Podstawiamy z uwagą na znaki:<br>\\( 3(-1)^3 - (-1) = 3(-1) + 1 = -3 + 1 = -2 \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz wartość wyrażenia \\( (x - y)(x + y) \\) dla \\( x = 5, y = 2 \\)',
+        rozwiazanie: 'Możemy wstawić wartości bezpośrednio lub użyć wzoru skróconego mnożenia \\( x^2 - y^2 \\). Wstawiając prosto:<br>\\( (5 - 2)(5 + 2) = 3 \\cdot 7 = 21 \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Podaj wartość wyrażenia \\( -x^2 \\) dla \\( x = -3 \\)',
+        rozwiazanie: 'Najpierw podnosimy do kwadratu, potem dodajemy minus z przodu:<br>\\( -(-3)^2 = -(9) = -9 \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz wartość \\( \\sqrt{x^2 + y^2} \\) dla \\( x = 3, y = 4 \\)',
+        rozwiazanie: 'Podstawiamy do twierdzenia Pitagorasa:<br>\\( \\sqrt{3^2 + 4^2} = \\sqrt{9 + 16} = \\sqrt{25} = 5 \\)'
+    },
+    {
+        poddzial: 'Wartość liczbowa wyrażenia',
+        tresc: 'Oblicz wartość ułamka \\( \\frac{2x - 1}{x^2 + 1} \\) dla \\( x = 0 \\)',
+        rozwiazanie: 'Wstawiamy zero za x:<br>\\( \\frac{2(0) - 1}{0^2 + 1} = \\frac{-1}{1} = -1 \\)'
+    }
+];
