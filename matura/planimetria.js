60ee3925a814b518368194a9ef17417f077e98c1
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:50:02 +0100

    Update planimetria.js

--- matura/planimetria.js
+++ matura/planimetria.js
@@ -1,1 +1,157 @@
+bazaZadan['planimetria'] = [
+    // PODDZIAŁ 1: Trójkąty i ich własności (10 zadań)
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Dwa kąty w trójkącie mają miary 45 stopni i 70 stopni. Jaka jest miara trzeciego kąta?',
+        rozwiazanie: 'Suma kątów w trójkącie to zawsze 180 stopni.<br>\\( 180^\\circ - (45^\\circ + 70^\\circ) = 180^\\circ - 115^\\circ = 65^\\circ \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Sprawdź, czy z odcinków o długościach 4, 5 i 10 można zbudować trójkąt.',
+        rozwiazanie: 'Korzystamy z nierówności trójkąta: suma długości dwóch krótszych boków musi być większa od najdłuższego.<br>\\( 4 + 5 = 9 \\).<br>Ponieważ 9 nie jest większe od 10, trójkąta nie da się zbudować.'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Oblicz wysokość trójkąta równobocznego o boku długości 6.',
+        rozwiazanie: 'Korzystamy ze wzoru na wysokość trójkąta równobocznego: \\( h = \\frac{a\\sqrt{3}}{2} \\).<br>\\( h = \\frac{6\\sqrt{3}}{2} = 3\\sqrt{3} \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Oblicz pole trójkąta równobocznego o boku długości 4.',
+        rozwiazanie: 'Wzór na pole trójkąta równobocznego: \\( P = \\frac{a^2\\sqrt{3}}{4} \\).<br>\\( P = \\frac{4^2\\sqrt{3}}{4} = \\frac{16\\sqrt{3}}{4} = 4\\sqrt{3} \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'W trójkącie prostokątnym przyprostokątne mają 6 i 8. Oblicz promień okręgu opisanego na tym trójkącie.',
+        rozwiazanie: 'Promień okręgu opisanego na trójkącie prostokątnym to połowa przeciwprostokątnej.<br>Z Pitagorasa: \\( c^2 = 6^2 + 8^2 = 36 + 64 = 100 \\Rightarrow c = 10 \\).<br>\\( R = \\frac{10}{2} = 5 \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Trójkąt \\( A_1 B_1 C_1 \\) jest podobny do trójkąta ABC w skali k = 3. Bok AB ma długość 5. Oblicz długość boku \\( A_1 B_1 \\).',
+        rozwiazanie: 'Skala podobieństwa to stosunek długości odpowiednich boków: \\( \\frac{A_1 B_1}{AB} = k \\).<br>\\( A_1 B_1 = k \\cdot AB = 3 \\cdot 5 = 15 \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Trójkąt T1 ma pole 20. Trójkąt T2 jest do niego podobny w skali k = 2. Oblicz pole trójkąta T2.',
+        rozwiazanie: 'Stosunek pól figur podobnych jest równy kwadratowi skali podobieństwa: \\( \\frac{P_2}{P_1} = k^2 \\).<br>\\( P_2 = k^2 \\cdot P_1 = 2^2 \\cdot 20 = 4 \\cdot 20 = 80 \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Obwód trójkąta równoramiennego wynosi 24. Ramię jest dwa razy dłuższe od podstawy. Oblicz długość podstawy.',
+        rozwiazanie: 'Podstawa to a, ramiona to 2a.<br>Obwód: \\( a + 2a + 2a = 24 \\)<br>\\( 5a = 24 \\Rightarrow a = 4.8 \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'Dwa boki trójkąta mają długość 4 i 6, a kąt między nimi ma 150 stopni. Oblicz pole tego trójkąta.',
+        rozwiazanie: 'Wzór: \\( P = \\frac{1}{2}ab\\sin\\alpha \\).<br>\\( \\sin 150^\\circ = \\sin(180^\\circ - 30^\\circ) = \\sin 30^\\circ = 0.5 \\).<br>\\( P = \\frac{1}{2} \\cdot 4 \\cdot 6 \\cdot 0.5 = 6 \\)'
+    },
+    {
+        poddzial: 'Trójkąty i ich własności',
+        tresc: 'W trójkącie prostokątnym wysokość poprowadzona z wierzchołka kąta prostego dzieli przeciwprostokątną na odcinki długości 2 i 8. Oblicz tę wysokość.',
+        rozwiazanie: 'Kwadrat wysokości opuszczonej na przeciwprostokątną jest równy iloczynowi długości odcinków, na jakie ją dzieli: \\( h^2 = x \\cdot y \\).<br>\\( h^2 = 2 \\cdot 8 = 16 \\Rightarrow h = 4 \\)'
+    },
 
+    // PODDZIAŁ 2: Czworokąty (10 zadań)
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Oblicz przekątną kwadratu o boku 5.',
+        rozwiazanie: 'Wzór na przekątną kwadratu: \\( d = a\\sqrt{2} \\).<br>\\( d = 5\\sqrt{2} \\)'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Przekątna kwadratu ma długość 8. Oblicz jego pole.',
+        rozwiazanie: 'Pole kwadratu z przekątnej: \\( P = \\frac{d^2}{2} \\).<br>\\( P = \\frac{8^2}{2} = \\frac{64}{2} = 32 \\)'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Obwód prostokąta wynosi 30, a jeden bok jest o 3 dłuższy od drugiego. Oblicz pole prostokąta.',
+        rozwiazanie: 'Boki to a oraz \\( a+3 \\).<br>Obwód: \\( 2a + 2(a+3) = 30 \\Rightarrow 4a + 6 = 30 \\Rightarrow 4a = 24 \\Rightarrow a = 6 \\).<br>Drugi bok to 9. Pole: \\( P = 6 \\cdot 9 = 54 \\)'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Przekątne rombu mają długości 6 i 10. Oblicz pole tego rombu.',
+        rozwiazanie: 'Pole rombu z przekątnych: \\( P = \\frac{e \\cdot f}{2} \\).<br>\\( P = \\frac{6 \\cdot 10}{2} = 30 \\)'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Kąt ostry rombu ma 50 stopni. Podaj miarę kąta rozwartego.',
+        rozwiazanie: 'Suma kątów przy jednym boku rombu wynosi 180 stopni.<br>\\( 180^\\circ - 50^\\circ = 130^\\circ \\)'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'W równoległoboku jeden z kątów jest 3 razy większy od drugiego. Oblicz miary tych kątów.',
+        rozwiazanie: 'Kąty to \\( \\alpha \\) i \\( 3\\alpha \\). Ich suma to 180 stopni.<br>\\( 4\\alpha = 180^\\circ \\Rightarrow \\alpha = 45^\\circ \\).<br>Drugi kąt to \\( 3 \\cdot 45^\\circ = 135^\\circ \\).'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Boki równoległoboku mają 5 i 8, a kąt między nimi wynosi 30 stopni. Oblicz pole.',
+        rozwiazanie: 'Pole równoległoboku: \\( P = a \\cdot b \\cdot \\sin\\alpha \\).<br>\\( P = 5 \\cdot 8 \\cdot \\sin 30^\\circ = 40 \\cdot 0.5 = 20 \\)'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'W trapezie równoramiennym kąt przy dolnej podstawie ma 70 stopni. Jakie miary mają pozostałe kąty?',
+        rozwiazanie: 'Trapez równoramienny ma równe kąty przy podstawach. Przy dolnej: 70 i 70. Suma kątów przy ramieniu to 180.<br>Przy górnej: \\( 180^\\circ - 70^\\circ = 110^\\circ \\). Kąty to 70, 70, 110, 110.'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Podstawy trapezu mają długości 4 i 10, a wysokość wynosi 5. Oblicz pole.',
+        rozwiazanie: 'Wzór: \\( P = \\frac{a+b}{2} \\cdot h \\).<br>\\( P = \\frac{4+10}{2} \\cdot 5 = 7 \\cdot 5 = 35 \\)'
+    },
+    {
+        poddzial: 'Czworokąty',
+        tresc: 'Czworokąt ABCD jest wpisany w okrąg. Kąt przy wierzchołku A ma miarę 80 stopni. Jaką miarę ma kąt przy wierzchołku C?',
+        rozwiazanie: 'Na czworokącie można opisać okrąg tylko wtedy, gdy suma jego przeciwległych kątów wynosi 180 stopni.<br>\\( 180^\\circ - 80^\\circ = 100^\\circ \\)'
+    },
+
+    // PODDZIAŁ 3: Okręgi i koła (10 zadań)
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Oblicz obwód koła o promieniu 4.',
+        rozwiazanie: 'Wzór: \\( L = 2\\pi r \\).<br>\\( L = 2\\pi \\cdot 4 = 8\\pi \\)'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Pole koła wynosi \\( 25\\pi \\). Oblicz jego promień.',
+        rozwiazanie: 'Wzór: \\( P = \\pi r^2 \\).<br>\\( \\pi r^2 = 25\\pi \\Rightarrow r^2 = 25 \\Rightarrow r = 5 \\)'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Oblicz pole wycinka koła o promieniu 6, opartego na kącie środkowym 60 stopni.',
+        rozwiazanie: 'Kąt pełny to 360 stopni. 60 stopni to \\( \\frac{1}{6} \\) koła.<br>Pole całego koła: \\( \\pi \\cdot 6^2 = 36\\pi \\).<br>Pole wycinka: \\( \\frac{1}{6} \\cdot 36\\pi = 6\\pi \\)'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Kąt środkowy ma miarę 120 stopni. Oblicz miarę kąta wpisanego opartego na tym samym łuku.',
+        rozwiazanie: 'Kąt wpisany jest zawsze dwa razy mniejszy od kąta środkowego opartego na tym samym łuku.<br>\\( 120^\\circ : 2 = 60^\\circ \\)'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Kąt wpisany jest oparty na półokręgu. Jaką ma miarę?',
+        rozwiazanie: 'Kąt wpisany oparty na półokręgu (średnicy) to zawsze kąt prosty, czyli ma miarę 90 stopni.'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Oblicz długość łuku opartego na kącie 90 stopni w okręgu o promieniu 8.',
+        rozwiazanie: '90 stopni to 1/4 pełnego okręgu.<br>Obwód okręgu: \\( L = 2\\pi \\cdot 8 = 16\\pi \\).<br>Długość łuku: \\( \\frac{1}{4} \\cdot 16\\pi = 4\\pi \\)'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Promień okręgu wynosi 5, a odległość środka tego okręgu od prostej wynosi 3. Ile punktów wspólnych ma ta prosta z okręgiem?',
+        rozwiazanie: 'Odległość od środka (d = 3) jest mniejsza niż promień (r = 5). Prosta przecina okrąg (jest sieczną).<br>Ma dokładnie 2 punkty wspólne.'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Dwa okręgi o promieniach 3 i 7 są styczne zewnętrznie. Oblicz odległość między ich środkami.',
+        rozwiazanie: 'Okręgi są styczne zewnętrznie, więc odległość środków to suma promieni.<br>\\( |S_1 S_2| = r_1 + r_2 = 3 + 7 = 10 \\)'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Oblicz promień okręgu wpisanego w trójkąt równoboczny o wysokości równej 12.',
+        rozwiazanie: 'Promień okręgu wpisanego w trójkąt równoboczny to \\( \\frac{1}{3} \\) jego wysokości.<br>\\( r = \\frac{1}{3} \\cdot 12 = 4 \\)'
+    },
+    {
+        poddzial: 'Okręgi i koła',
+        tresc: 'Z punktu P poprowadzono styczną do okręgu o środku S. Punkt styczności to A. Jaki kąt tworzy odcinek SA (promień) ze styczną PA?',
+        rozwiazanie: 'Zgodnie z własnościami stycznej do okręgu, promień poprowadzony do punktu styczności jest zawsze do niej prostopadły.<br>Kąt wynosi 90 stopni.'
+    }
+];
