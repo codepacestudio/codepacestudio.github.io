00793574268564d0bdcc991bfad97750ebcb8a34
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:47:54 +0100

    Update ciagi.js

--- matura/ciagi.js
+++ matura/ciagi.js
@@ -1,1 +1,152 @@
-
+bazaZadan['ciagi'] = [
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Oblicz trzeci wyraz ciągu określonego wzorem: \\( a_n = 2n - 5 \\)',
+        rozwiazanie: 'Podstawiamy za n liczbę 3:<br>\\( a_3 = 2 \\cdot 3 - 5 = 6 - 5 = 1 \\)'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Sprawdź, czy ciąg o wzorze \\( a_n = 3n + 1 \\) jest rosnący.',
+        rozwiazanie: 'Liczymy różnicę \\( a_{n+1} - a_n \\):<br>\\( a_{n+1} = 3(n+1) + 1 = 3n + 4 \\)<br>\\( a_{n+1} - a_n = (3n + 4) - (3n + 1) = 3 \\)<br>Różnica jest stała i dodatnia, więc ciąg jest rosnący.'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Który wyraz ciągu \\( a_n = n^2 - 4 \\) jest równy 5?',
+        rozwiazanie: 'Przyrównujemy wzór do 5:<br>\\( n^2 - 4 = 5 \\Rightarrow n^2 = 9 \\)<br>Skoro n musi być liczbą naturalną dodatnią (numer wyrazu), to \\( n = 3 \\).'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Oblicz sumę dwóch pierwszych wyrazów ciągu \\( a_n = \\frac{n+1}{n} \\).',
+        rozwiazanie: 'Liczymy pierwszy i drugi wyraz:<br>\\( a_1 = \\frac{1+1}{1} = 2 \\)<br>\\( a_2 = \\frac{2+1}{2} = \\frac{3}{2} = 1.5 \\)<br>Suma: \\( 2 + 1.5 = 3.5 \\)'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Ile wyrazów dodatnich ma ciąg określony wzorem \\( a_n = -n^2 + 10 \\)?',
+        rozwiazanie: 'Rozwiązujemy nierówność \\( a_n > 0 \\):<br>\\( -n^2 + 10 > 0 \\Rightarrow n^2 < 10 \\).<br>Liczby naturalne dodatnie spełniające ten warunek to 1, 2, 3. Ciąg ma 3 wyrazy dodatnie.'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Zbadaj monotoniczność ciągu \\( a_n = -2n + 5 \\).',
+        rozwiazanie: 'Liczymy \\( a_{n+1} - a_n \\):<br>\\( -2(n+1) + 5 - (-2n + 5) = -2n - 2 + 5 + 2n - 5 = -2 \\).<br>Różnica jest ujemna, ciąg jest malejący.'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Który wyraz ciągu \\( a_n = 5n - 12 \\) jest równy 18?',
+        rozwiazanie: 'Układamy równanie:<br>\\( 5n - 12 = 18 \\Rightarrow 5n = 30 \\Rightarrow n = 6 \\).<br>Odpowiedź: szósty wyraz.'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Oblicz wartość wyrażenia \\( a_5 - a_4 \\) dla ciągu \\( a_n = n^2 \\).',
+        rozwiazanie: 'Liczymy oba wyrazy:<br>\\( a_5 = 5^2 = 25 \\)<br>\\( a_4 = 4^2 = 16 \\)<br>Różnica: \\( 25 - 16 = 9 \\)'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Czy liczba 10 jest wyrazem ciągu \\( a_n = 2n^2 + 2 \\)?',
+        rozwiazanie: 'Przyrównujemy:<br>\\( 2n^2 + 2 = 10 \\Rightarrow 2n^2 = 8 \\Rightarrow n^2 = 4 \\)<br>Zatem \\( n = 2 \\). Tak, jest to drugi wyraz tego ciągu.'
+    },
+    {
+        poddzial: 'Własności ciągów',
+        tresc: 'Dany jest ciąg \\( a_n = (-1)^n \\cdot n \\). Oblicz sumę \\( a_1 + a_2 + a_3 \\).',
+        rozwiazanie: 'Liczymy po kolei:<br>\\( a_1 = (-1)^1 \\cdot 1 = -1 \\)<br>\\( a_2 = (-1)^2 \\cdot 2 = 2 \\)<br>\\( a_3 = (-1)^3 \\cdot 3 = -3 \\)<br>Suma: \\( -1 + 2 - 3 = -2 \\)'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Oblicz dziesiąty wyraz ciągu arytmetycznego, w którym \\( a_1 = 3 \\) i różnica \\( r = 2 \\).',
+        rozwiazanie: 'Korzystamy ze wzoru na n-ty wyraz: \\( a_n = a_1 + (n-1)r \\).<br>\\( a_{10} = 3 + 9 \\cdot 2 = 3 + 18 = 21 \\)'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Znajdź różnicę ciągu arytmetycznego, jeśli \\( a_1 = 5 \\) i \\( a_5 = 17 \\).',
+        rozwiazanie: 'Wzór: \\( a_5 = a_1 + 4r \\).<br>Podstawiamy: \\( 17 = 5 + 4r \\Rightarrow 12 = 4r \\Rightarrow r = 3 \\)'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Oblicz sumę dziesięciu pierwszych wyrazów ciągu arytmetycznego, w którym \\( a_1 = 2 \\), \\( a_{10} = 20 \\).',
+        rozwiazanie: 'Korzystamy ze wzoru na sumę: \\( S_n = \\frac{a_1 + a_n}{2} \\cdot n \\).<br>\\( S_{10} = \\frac{2 + 20}{2} \\cdot 10 = \\frac{22}{2} \\cdot 10 = 11 \\cdot 10 = 110 \\)'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Wyznacz x, dla którego liczby 2, x, 10 tworzą ciąg arytmetyczny.',
+        rozwiazanie: 'Środkowy wyraz to średnia arytmetyczna skrajnych:<br>\\( x = \\frac{2 + 10}{2} = \\frac{12}{2} = 6 \\)'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Ile wyrazów ma skończony ciąg arytmetyczny: 4, 7, 10, ..., 31?',
+        rozwiazanie: 'Widzimy, że \\( a_1 = 4 \\) i \\( r = 3 \\). Szukamy n dla \\( a_n = 31 \\).<br>\\( 31 = 4 + (n-1) \\cdot 3 \\)<br>\\( 27 = 3(n-1) \\Rightarrow 9 = n-1 \\Rightarrow n = 10 \\). Ciąg ma 10 wyrazów.'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'W ciągu arytmetycznym \\( a_3 = 8 \\) i \\( a_7 = 20 \\). Oblicz \\( a_1 \\).',
+        rozwiazanie: 'Różnica między siódmym a trzecim wyrazem to 4r.<br>\\( 4r = 20 - 8 = 12 \\Rightarrow r = 3 \\).<br>Cofamy się do pierwszego wyrazu: \\( a_1 = a_3 - 2r = 8 - 2 \\cdot 3 = 2 \\).'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Zbadaj, czy ciąg \\( a_n = 4n - 3 \\) jest ciągiem arytmetycznym.',
+        rozwiazanie: 'Liczymy różnicę \\( a_{n+1} - a_n \\):<br>\\( (4(n+1) - 3) - (4n - 3) = 4n + 4 - 3 - 4n + 3 = 4 \\).<br>Różnica jest stała (nie zależy od n), więc ciąg jest arytmetyczny, a jego różnica r wynosi 4.'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Oblicz sumę stu pierwszych liczb naturalnych dodatnich (1, 2, 3... 100).',
+        rozwiazanie: 'To ciąg arytmetyczny o \\( a_1 = 1 \\), \\( a_{100} = 100 \\), \\( n = 100 \\).<br>\\( S_{100} = \\frac{1 + 100}{2} \\cdot 100 = 101 \\cdot 50 = 5050 \\)'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Pierwszy wyraz ciągu arytmetycznego wynosi -5, a różnica to 3. Który wyraz tego ciągu jest równy 22?',
+        rozwiazanie: 'Podstawiamy do wzoru na n-ty wyraz:<br>\\( 22 = -5 + (n-1) \\cdot 3 \\)<br>\\( 27 = 3(n-1) \\Rightarrow 9 = n-1 \\Rightarrow n = 10 \\). Odpowiedź: dziesiąty wyraz.'
+    },
+    {
+        poddzial: 'Ciąg arytmetyczny',
+        tresc: 'Suma trzech pierwszych wyrazów ciągu arytmetycznego wynosi 15. Oblicz drugi wyraz tego ciągu.',
+        rozwiazanie: 'Wyrazy to: \\( a_2 - r \\), \\( a_2 \\), \\( a_2 + r \\).<br>Ich suma: \\( a_2 - r + a_2 + a_2 + r = 3a_2 \\).<br>Zatem \\( 3a_2 = 15 \\Rightarrow a_2 = 5 \\).'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Oblicz piąty wyraz ciągu geometrycznego, w którym \\( a_1 = 2 \\) i iloraz \\( q = 3 \\).',
+        rozwiazanie: 'Korzystamy ze wzoru: \\( a_n = a_1 \\cdot q^{n-1} \\).<br>\\( a_5 = 2 \\cdot 3^4 = 2 \\cdot 81 = 162 \\)'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Wyznacz iloraz ciągu geometrycznego o wyrazach dodatnich, w którym \\( a_1 = 4 \\) i \\( a_3 = 36 \\).',
+        rozwiazanie: 'Wzór: \\( a_3 = a_1 \\cdot q^2 \\).<br>\\( 36 = 4 \\cdot q^2 \\Rightarrow q^2 = 9 \\).<br>Skoro wyrazy są dodatnie, to \\( q = 3 \\).'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Wyznacz x, dla którego liczby 3, x, 27 tworzą ciąg geometryczny dodatni.',
+        rozwiazanie: 'Kwadrat środkowego wyrazu jest iloczynem skrajnych:<br>\\( x^2 = 3 \\cdot 27 = 81 \\).<br>Skoro ciąg jest dodatni, \\( x = 9 \\).'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Oblicz sumę czterech pierwszych wyrazów ciągu geometrycznego, w którym \\( a_1 = 1 \\), \\( q = 2 \\).',
+        rozwiazanie: 'Można użyć wzoru lub policzyć na piechotę (to tylko 4 wyrazy): 1, 2, 4, 8.<br>Suma: \\( 1 + 2 + 4 + 8 = 15 \\).'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'W ciągu geometrycznym \\( a_2 = 6 \\) i \\( a_4 = 54 \\) (q > 0). Oblicz \\( a_1 \\).',
+        rozwiazanie: 'Najpierw szukamy q: \\( a_4 = a_2 \\cdot q^2 \\Rightarrow 54 = 6 \\cdot q^2 \\Rightarrow q^2 = 9 \\Rightarrow q = 3 \\).<br>Cofamy się do pierwszego: \\( a_1 = \\frac{a_2}{q} = \\frac{6}{3} = 2 \\).'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Sprawdź, czy liczby 2, 4, 8 tworzą ciąg geometryczny.',
+        rozwiazanie: 'Sprawdzamy stały iloraz: \\( \\frac{4}{2} = 2 \\) oraz \\( \\frac{8}{4} = 2 \\).<br>Iloraz jest stały, więc jest to ciąg geometryczny.'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Trzeci wyraz ciągu to 12, a czwarty to 24. Oblicz iloraz q.',
+        rozwiazanie: 'Iloraz q to stosunek wyrazu następnego do poprzedniego.<br>\\( q = \\frac{a_4}{a_3} = \\frac{24}{12} = 2 \\).'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Dany jest ciąg geometryczny o wzorze ogólnym \\( a_n = 5 \\cdot 2^n \\). Oblicz iloraz tego ciągu.',
+        rozwiazanie: 'Liczymy dwa kolejne wyrazy: \\( a_1 = 5 \\cdot 2^1 = 10 \\), \\( a_2 = 5 \\cdot 2^2 = 20 \\).<br>Iloraz: \\( q = \\frac{a_2}{a_1} = \\frac{20}{10} = 2 \\).<br>Zawsze jest to liczba potęgowana przez n we wzorze.'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'Oblicz iloczyn trzech pierwszych wyrazów ciągu geometrycznego, w którym \\( a_1 = 2 \\), \\( q = 3 \\).',
+        rozwiazanie: 'Wyznaczamy wyrazy: \\( a_1 = 2 \\), \\( a_2 = 6 \\), \\( a_3 = 18 \\).<br>Iloczyn: \\( 2 \\cdot 6 \\cdot 18 = 12 \\cdot 18 = 216 \\).'
+    },
+    {
+        poddzial: 'Ciąg geometryczny',
+        tresc: 'W ciągu geometrycznym iloraz wynosi 0.5, a drugi wyraz to 10. Oblicz pierwszy wyraz.',
+        rozwiazanie: 'Skoro \\( a_2 = a_1 \\cdot q \\), to \\( 10 = a_1 \\cdot 0.5 \\).<br>Dzielimy obustronnie przez 0.5 (co odpowiada mnożeniu przez 2).<br>\\( a_1 = 20 \\).'
+    }
+];
