b2824fd8687e80eb7d1ac2dc20d53a2b6da4ad52
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:44:55 +0100

    Update uklady.js

--- matura/uklady.js
+++ matura/uklady.js
@@ -1,1 +1,152 @@
-
+bazaZadan['uklady'] = [
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ równań: \\( x + y = 5 \\) oraz \\( x - y = 1 \\).',
+        rozwiazanie: 'Dodajemy równania stronami (metoda przeciwnych współczynników):<br>\\( 2x = 6 \\Rightarrow x = 3 \\)<br>Podstawiamy do pierwszego: \\( 3 + y = 5 \\Rightarrow y = 2 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ: \\( 2x + y = 7 \\) oraz \\( 3x - y = 8 \\).',
+        rozwiazanie: 'Dodajemy stronami, bo mamy y oraz -y:<br>\\( 5x = 15 \\Rightarrow x = 3 \\)<br>Podstawiamy x do pierwszego: \\( 2(3) + y = 7 \\Rightarrow 6 + y = 7 \\Rightarrow y = 1 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ metodą podstawiania: \\( x - 2y = 4 \\) i \\( 2x + y = 13 \\).',
+        rozwiazanie: 'Z pierwszego wyznaczamy x: \\( x = 4 + 2y \\).<br>Podstawiamy do drugiego: \\( 2(4 + 2y) + y = 13 \\)<br>\\( 8 + 4y + y = 13 \\)<br>\\( 5y = 5 \\Rightarrow y = 1 \\)<br>Wracamy do x: \\( x = 4 + 2(1) = 6 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ: \\( y = 2x - 1 \\) oraz \\( x + y = 8 \\).',
+        rozwiazanie: 'Mamy gotowy y, wstawiamy do drugiego równania:<br>\\( x + (2x - 1) = 8 \\)<br>\\( 3x = 9 \\Rightarrow x = 3 \\)<br>Liczymy y: \\( y = 2(3) - 1 = 5 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ: \\( 4x - y = 10 \\) i \\( y = x + 2 \\).',
+        rozwiazanie: 'Podstawiamy y do pierwszego:<br>\\( 4x - (x + 2) = 10 \\)<br>\\( 3x - 2 = 10 \\Rightarrow 3x = 12 \\Rightarrow x = 4 \\)<br>Liczymy y: \\( y = 4 + 2 = 6 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Znajdź rozwiązanie: \\( 5x + 2y = 12 \\) oraz \\( 3x - 2y = 4 \\).',
+        rozwiazanie: 'Dodajemy stronami:<br>\\( 8x = 16 \\Rightarrow x = 2 \\)<br>Podstawiamy do pierwszego: \\( 5(2) + 2y = 12 \\)<br>\\( 10 + 2y = 12 \\Rightarrow 2y = 2 \\Rightarrow y = 1 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ: \\( 0.5x + y = 4 \\) i \\( x - 0.5y = 3 \\).',
+        rozwiazanie: 'Mnożymy drugie równanie przez 2: \\( 2x - y = 6 \\).<br>Dodajemy do pierwszego:\\( 2.5x = 10 \\Rightarrow x = 4 \\).<br>Liczymy y: \\( 0.5(4) + y = 4 \\Rightarrow 2 + y = 4 \\Rightarrow y = 2 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ: \\( x + 3y = 0 \\) i \\( 2x - y = 7 \\).',
+        rozwiazanie: 'Z pierwszego wyznaczamy x: \\( x = -3y \\).<br>Podstawiamy do drugiego: \\( 2(-3y) - y = 7 \\)<br>\\( -7y = 7 \\Rightarrow y = -1 \\)<br>Wtedy \\( x = -3(-1) = 3 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ: \\( 2x - 3y = 1 \\) i \\( 5x + 3y = 13 \\).',
+        rozwiazanie: 'Dodajemy stronami:<br>\\( 7x = 14 \\Rightarrow x = 2 \\).<br>Podstawiamy do pierwszego: \\( 2(2) - 3y = 1 \\)<br>\\( 4 - 3y = 1 \\Rightarrow -3y = -3 \\Rightarrow y = 1 \\)'
+    },
+    {
+        poddzial: 'Rozwiązywanie układów równań',
+        tresc: 'Rozwiąż układ: \\( 3x + y = 4 \\) i \\( x = y + 4 \\).',
+        rozwiazanie: 'Podstawiamy x do pierwszego równania:<br>\\( 3(y + 4) + y = 4 \\)<br>\\( 3y + 12 + y = 4 \\)<br>\\( 4y = -8 \\Rightarrow y = -2 \\)<br>Liczymy x: \\( x = -2 + 4 = 2 \\)'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Określ liczbę rozwiązań układu: \\( y = 2x + 1 \\) i \\( y = 2x - 3 \\).',
+        rozwiazanie: 'Proste mają ten sam współczynnik kierunkowy (a = 2), ale różny wyraz wolny (b). Są równoległe i nie mają punktów wspólnych.<br>Układ jest sprzeczny, ma 0 rozwiązań.'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Podaj liczbę rozwiązań układu: \\( y = -x + 4 \\) i \\( y = x - 2 \\).',
+        rozwiazanie: 'Współczynniki kierunkowe są różne (-1 i 1). Proste przecinają się w jednym punkcie.<br>Układ jest oznaczony, ma dokładnie 1 rozwiązanie.'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Określ rodzaj układu: \\( 2x - y = 3 \\) i \\( 4x - 2y = 6 \\).',
+        rozwiazanie: 'Po podzieleniu drugiego równania przez 2 otrzymujemy \\( 2x - y = 3 \\). To są dwa identyczne równania (ta sama prosta).<br>Układ jest nieoznaczony, ma nieskończenie wiele rozwiązań.'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Dla jakiego "a" układ \\( x + ay = 5 \\) oraz \\( 2x - 4y = 10 \\) ma nieskończenie wiele rozwiązań?',
+        rozwiazanie: 'Równania muszą być proporcjonalne. Drugie to podwojone pierwsze.<br>Skoro wyraz przy x to 2, a wyraz wolny to 10, to \\( 2a = -4 \\).<br>Stąd \\( a = -2 \\).'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Ile rozwiązań ma układ: \\( y = 3x \\) i \\( y = 3x + 2 \\)?',
+        rozwiazanie: 'Proste są równoległe. Nie mają punktu wspólnego.<br>Zero rozwiązań (układ sprzeczny).'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Czy punkt (1, 2) należy do zbioru rozwiązań układu \\( y = 2x \\) i \\( y = -x + 3 \\)?',
+        rozwiazanie: 'Sprawdzamy pierwsze równanie: \\( 2 = 2 \\cdot 1 \\) (Prawda).<br>Sprawdzamy drugie: \\( 2 = -1 + 3 \\) (Prawda).<br>Tak, ten punkt jest rozwiązaniem układu.'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Rozwiąż graficznie układ prostych: \\( y = x \\) i \\( y = -x + 4 \\).',
+        rozwiazanie: 'Podstawiamy jedno do drugiego: \\( x = -x + 4 \\)<br>\\( 2x = 4 \\Rightarrow x = 2 \\)<br>Wtedy \\( y = 2 \\). Rozwiązaniem jest punkt przecięcia (2, 2).'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Wyznacz punkt przecięcia prostych: \\( x + y = 5 \\) i \\( x = 2 \\).',
+        rozwiazanie: 'Podstawiamy x = 2 do pierwszego równania:<br>\\( 2 + y = 5 \\Rightarrow y = 3 \\).<br>Punkt przecięcia to (2, 3).'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Czy układ \\( x + y = 0 \\) i \\( x - y = 0 \\) ma nieskończenie wiele rozwiązań?',
+        rozwiazanie: 'Dodając równania stronami otrzymujemy \\( 2x = 0 \\Rightarrow x = 0 \\).<br>Wtedy \\( y = 0 \\). Proste przecinają się tylko w punkcie (0,0). Ma dokładnie jedno rozwiązanie.'
+    },
+    {
+        poddzial: 'Interpretacja geometryczna',
+        tresc: 'Znajdź rozwiązanie układu \\( y = 5 \\) i \\( x = -3 \\).',
+        rozwiazanie: 'Równania te reprezentują proste prostopadłe do osi układu.<br>Ich punktem przecięcia, a zarazem jedynym rozwiązaniem jest po prostu (-3, 5).'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Suma dwóch liczb to 20, a ich różnica to 4. Znajdź te liczby.',
+        rozwiazanie: 'Układamy równania: \\( x + y = 20 \\) i \\( x - y = 4 \\).<br>Dodajemy stronami: \\( 2x = 24 \\Rightarrow x = 12 \\).<br>Wtedy \\( 12 + y = 20 \\Rightarrow y = 8 \\).'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Trzy bułki i jeden chleb kosztują 8 zł, a pięć bułek i chleb kosztują 11 zł. Ile kosztuje bułka?',
+        rozwiazanie: 'x - bułka, y - chleb.<br>\\( 3x + y = 8 \\) oraz \\( 5x + y = 11 \\).<br>Odejmujemy pierwsze od drugiego: \\( 2x = 3 \\Rightarrow x = 1.50 \\) zł.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Za 2 zeszyty i długopis zapłacono 15 zł. Zeszyt jest o 3 zł droższy od długopisu. Ile kosztuje długopis?',
+        rozwiazanie: 'z - zeszyt, d - długopis.<br>\\( 2z + d = 15 \\) oraz \\( z = d + 3 \\).<br>Podstawiamy: \\( 2(d + 3) + d = 15 \\)<br>\\( 3d + 6 = 15 \\Rightarrow 3d = 9 \\Rightarrow d = 3 \\) zł.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Ojciec jest 3 razy starszy od syna. Razem mają 48 lat. Ile lat ma syn?',
+        rozwiazanie: 'o - ojciec, s - syn.<br>\\( o = 3s \\) i \\( o + s = 48 \\).<br>Podstawiamy: \\( 3s + s = 48 \\Rightarrow 4s = 48 \\Rightarrow s = 12 \\) lat.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'W klasie jest 30 uczniów. Dziewcząt jest o 4 więcej niż chłopców. Ilu chłopców jest w klasie?',
+        rozwiazanie: 'd - dziewczęta, c - chłopcy.<br>\\( d + c = 30 \\) i \\( d = c + 4 \\).<br>Podstawiamy: \\( (c + 4) + c = 30 \\Rightarrow 2c = 26 \\Rightarrow c = 13 \\) chłopców.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Obwód prostokąta wynosi 24 cm. Jego długość jest dwa razy większa od szerokości. Oblicz szerokość.',
+        rozwiazanie: 'a - długość, b - szerokość.<br>\\( 2a + 2b = 24 \\) i \\( a = 2b \\).<br>Podstawiamy: \\( 2(2b) + 2b = 24 \\Rightarrow 6b = 24 \\Rightarrow b = 4 \\) cm.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Kupiono 2 bilety normalne i 3 ulgowe za 60 zł. Bilet normalny był o 5 zł droższy od ulgowego. Podaj cenę biletu ulgowego.',
+        rozwiazanie: 'n - normalny, u - ulgowy.<br>\\( 2n + 3u = 60 \\) i \\( n = u + 5 \\).<br>Podstawiamy: \\( 2(u + 5) + 3u = 60 \\Rightarrow 5u + 10 = 60 \\Rightarrow 5u = 50 \\Rightarrow u = 10 \\) zł.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Różnica dwóch liczb wynosi 10. Jedna z nich jest 3 razy większa od drugiej. Jakie to liczby?',
+        rozwiazanie: '\\( x - y = 10 \\) i \\( x = 3y \\).<br>Podstawiamy: \\( 3y - y = 10 \\Rightarrow 2y = 10 \\Rightarrow y = 5 \\).<br>Wtedy \\( x = 3 \\cdot 5 = 15 \\). Liczby to 15 i 5.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'Suma cyfr pewnej liczby dwucyfrowej wynosi 9. Cyfra dziesiątek jest o 1 większa od cyfry jedności. Jaka to liczba?',
+        rozwiazanie: 'x - dziesiątki, y - jedności.<br>\\( x + y = 9 \\) i \\( x = y + 1 \\).<br>Podstawiamy: \\( y + 1 + y = 9 \\Rightarrow 2y = 8 \\Rightarrow y = 4 \\).<br>Wtedy x = 5. Szukana liczba to 54.'
+    },
+    {
+        poddzial: 'Zadania tekstowe',
+        tresc: 'W hotelu jest 20 pokoi dwu i trzyosobowych. Łącznie jest w nich 45 miejsc. Ile jest pokoi dwuosobowych?',
+        rozwiazanie: 'x - pokoje 2-osobowe, y - 3-osobowe.<br>\\( x + y = 20 \\) i \\( 2x + 3y = 45 \\).<br>Z pierwszego \\( y = 20 - x \\). Wstawiamy do drugiego:<br>\\( 2x + 3(20 - x) = 45 \\Rightarrow 2x + 60 - 3x = 45 \\Rightarrow -x = -15 \\Rightarrow x = 15 \\).'
+    }
+];
