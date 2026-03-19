9905763089cd1c40bed1fac31064eeb0ad16547b
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:46:26 +0100

    Update funkcje.js

--- matura/funkcje.js
+++ matura/funkcje.js
@@ -1,1 +1,152 @@
-
+bazaZadan['funkcje'] = [
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Wyznacz dziedzinę funkcji: \\( f(x) = \\frac{2}{x - 3} \\)',
+        rozwiazanie: 'Mianownik ułamka nie może być równy zero.<br>\\( x - 3 \\neq 0 \\Rightarrow x \\neq 3 \\)<br>Dziedzina: \\( D = \\mathbb{R} \\setminus \\{3\\} \\).'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Wyznacz dziedzinę funkcji: \\( f(x) = \\sqrt{x - 2} \\)',
+        rozwiazanie: 'Wyrażenie pod pierwiastkiem kwadratowym musi być nieujemne.<br>\\( x - 2 \\ge 0 \\Rightarrow x \\ge 2 \\)<br>Dziedzina: \\( x \\in [2, \\infty) \\).'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Oblicz miejsce zerowe funkcji: \\( f(x) = 3x - 12 \\)',
+        rozwiazanie: 'Przyrównujemy wzór funkcji do zera:<br>\\( 3x - 12 = 0 \\)<br>\\( 3x = 12 \\Rightarrow x = 4 \\)'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Wyznacz dziedzinę funkcji: \\( f(x) = \\frac{x}{x^2 - 4} \\)',
+        rozwiazanie: 'Mianownik nie może być zerem:<br>\\( x^2 - 4 \\neq 0 \\Rightarrow (x - 2)(x + 2) \\neq 0 \\)<br>\\( x \\neq 2 \\) oraz \\( x \\neq -2 \\)<br>Dziedzina: \\( \\mathbb{R} \\setminus \\{-2, 2\\} \\).'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Znajdź punkt przecięcia wykresu funkcji \\( f(x) = 5x - 7 \\) z osią OY.',
+        rozwiazanie: 'Punkt przecięcia z osią OY ma pierwszą współrzędną równą 0. Liczymy wartość funkcji dla x=0:<br>\\( f(0) = 5 \\cdot 0 - 7 = -7 \\)<br>Punkt to (0, -7).'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Podaj zbiór wartości funkcji kwadratowej: \\( f(x) = (x - 2)^2 + 3 \\)',
+        rozwiazanie: 'Ramiona paraboli są skierowane w górę (a = 1 > 0). Wierzchołek ma współrzędną y równą 3 (z postaci kanonicznej).<br>Zbiór wartości: \\( ZW = [3, \\infty) \\).'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Oblicz wartość funkcji \\( f(x) = x^2 - 3x \\) dla argumentu x = 2.',
+        rozwiazanie: 'Podstawiamy x = 2 do wzoru:<br>\\( f(2) = 2^2 - 3 \\cdot 2 = 4 - 6 = -2 \\)'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Sprawdź rachunkowo, czy punkt A(1, 4) należy do wykresu funkcji \\( f(x) = 2x + 2 \\).',
+        rozwiazanie: 'Wstawiamy x = 1 do wzoru i sprawdzamy, czy wynik to 4:<br>\\( f(1) = 2 \\cdot 1 + 2 = 4 \\)<br>Wynik się zgadza. Punkt należy do wykresu.'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Wyznacz dziedzinę funkcji: \\( f(x) = \\frac{1}{\\sqrt{x - 5}} \\)',
+        rozwiazanie: 'Wyrażenie pod pierwiastkiem musi być nieujemne, a cały mianownik różny od zera. Zatem musi być ostro większe od zera:<br>\\( x - 5 > 0 \\Rightarrow x > 5 \\)<br>Dziedzina: \\( (5, \\infty) \\).'
+    },
+    {
+        poddzial: 'Dziedzina i własności',
+        tresc: 'Jaka jest największa wartość funkcji \\( f(x) = -x^2 + 4 \\)?',
+        rozwiazanie: 'Parabola ma ramiona w dół. Największa wartość znajduje się w wierzchołku. Z postaci kanonicznej widać, że wierzchołek to W(0, 4).<br>Największa wartość wynosi 4.'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Znajdź wzór funkcji liniowej przechodzącej przez punkty A(0, 2) i B(2, 6).',
+        rozwiazanie: 'Wzór to \\( y = ax + b \\). Z punktu A(0, 2) wiemy od razu, że wyraz wolny \\( b = 2 \\).<br>Wstawiamy współrzędne B do równania: \\( 6 = a \\cdot 2 + 2 \\).<br>\\( 2a = 4 \\Rightarrow a = 2 \\).<br>Wzór funkcji: \\( y = 2x + 2 \\).'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Napisz równanie prostej równoległej do prostej \\( y = 3x - 1 \\) i przechodzącej przez punkt P(1, 5).',
+        rozwiazanie: 'Proste równoległe mają ten sam współczynnik kierunkowy. Zatem \\( a = 3 \\). Równanie ma postać \\( y = 3x + b \\).<br>Wstawiamy punkt P: \\( 5 = 3 \\cdot 1 + b \\Rightarrow b = 2 \\).<br>Równanie: \\( y = 3x + 2 \\).'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Podaj równanie prostej prostopadłej do prostej \\( y = \\frac{1}{2}x + 3 \\) przechodzącej przez początek układu współrzędnych.',
+        rozwiazanie: 'Współczynnik kierunkowy prostej prostopadłej to odwrotność z przeciwnym znakiem. Skoro \\( a_1 = \\frac{1}{2} \\), to \\( a_2 = -2 \\).<br>Przechodzi przez (0,0), więc \\( b = 0 \\).<br>Równanie: \\( y = -2x \\).'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Wyznacz miejsce zerowe funkcji liniowej \\( y = -2x + 8 \\).',
+        rozwiazanie: 'Przyrównujemy do zera:<br>\\( -2x + 8 = 0 \\)<br>\\( -2x = -8 \\Rightarrow x = 4 \\)'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Znajdź punkt przecięcia prostych: \\( y = 2x \\) i \\( y = x + 3 \\).',
+        rozwiazanie: 'Przyrównujemy prawe strony równań do siebie:<br>\\( 2x = x + 3 \\Rightarrow x = 3 \\).<br>Obliczamy y dla x=3: \\( y = 2 \\cdot 3 = 6 \\).<br>Punkt przecięcia to (3, 6).'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Dla jakiego m funkcja \\( f(x) = (2 - m)x + 5 \\) jest malejąca?',
+        rozwiazanie: 'Funkcja liniowa jest malejąca, gdy współczynnik przy x jest ujemny.<br>\\( 2 - m < 0 \\Rightarrow -m < -2 \\Rightarrow m > 2 \\).'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Jaki jest kąt nachylenia prostej \\( y = x + 4 \\) do osi OX?',
+        rozwiazanie: 'Współczynnik kierunkowy prostej (a) równa się tangensowi kąta nachylenia.<br>Tutaj \\( a = 1 \\), więc \\( \\tan(\\alpha) = 1 \\).<br>Kąt \\( \\alpha = 45^\\circ \\).'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Sprawdź, czy punkty A(0, 1), B(1, 3), C(2, 5) są współliniowe.',
+        rozwiazanie: 'Wyznaczamy wzór prostej AB: \\( b=1 \\), \\( 3 = a \\cdot 1 + 1 \\Rightarrow a = 2 \\). Równanie to \\( y = 2x + 1 \\).<br>Sprawdzamy punkt C: \\( 2 \\cdot 2 + 1 = 5 \\).<br>Zgadza się, punkty leżą na jednej prostej.'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Napisz równanie prostej przecinającej osie w punktach (2, 0) i (0, -4).',
+        rozwiazanie: 'Z punktu (0, -4) mamy od razu \\( b = -4 \\). Wzór to \\( y = ax - 4 \\).<br>Wstawiamy punkt (2, 0): \\( 0 = 2a - 4 \\Rightarrow 2a = 4 \\Rightarrow a = 2 \\).<br>Równanie prostej: \\( y = 2x - 4 \\).'
+    },
+    {
+        poddzial: 'Funkcja liniowa',
+        tresc: 'Dla jakiego m proste \\( y = mx - 5 \\) i \\( y = -2x + 1 \\) są równoległe?',
+        rozwiazanie: 'Proste są równoległe, gdy ich współczynniki kierunkowe są równe.<br>Odpowiedź: \\( m = -2 \\).'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Wyznacz współrzędne wierzchołka paraboli: \\( y = x^2 - 4x + 3 \\).',
+        rozwiazanie: 'Liczymy współrzędną p (x wierzchołka): \\( p = \\frac{-b}{2a} = \\frac{4}{2} = 2 \\).<br>Liczymy q (y wierzchołka): wstawiamy x=2 do wzoru: \\( y = 2^2 - 4 \\cdot 2 + 3 = 4 - 8 + 3 = -1 \\).<br>Wierzchołek to W(2, -1).'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Zapisz funkcję w postaci kanonicznej: \\( y = x^2 - 6x + 8 \\).',
+        rozwiazanie: 'Liczymy wierzchołek: \\( p = \\frac{6}{2} = 3 \\).<br>\\( q = 3^2 - 6 \\cdot 3 + 8 = 9 - 18 + 8 = -1 \\).<br>Wzór postaci kanonicznej \\( y = a(x-p)^2 + q \\). Skoro a=1, to mamy: \\( y = (x - 3)^2 - 1 \\).'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Znajdź miejsca zerowe funkcji: \\( f(x) = x^2 - 9 \\).',
+        rozwiazanie: 'Przyrównujemy do zera: \\( x^2 - 9 = 0 \\).<br>Ze wzoru skróconego mnożenia: \\( (x - 3)(x + 3) = 0 \\).<br>Miejsca zerowe to \\( x = 3 \\) oraz \\( x = -3 \\).'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Zapisz w postaci iloczynowej: \\( f(x) = x^2 - 5x + 6 \\).',
+        rozwiazanie: 'Liczymy deltę: \\( \\Delta = 25 - 24 = 1 \\). Pierwiastki: \\( x_1 = 2, x_2 = 3 \\).<br>Postać iloczynowa: \\( y = a(x - x_1)(x - x_2) \\). Tutaj a=1.<br>\\( f(x) = (x - 2)(x - 3) \\).'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Podaj równanie osi symetrii paraboli: \\( y = 2x^2 + 8x - 1 \\).',
+        rozwiazanie: 'Oś symetrii paraboli to pionowa prosta przechodząca przez wierzchołek, o równaniu \\( x = p \\).<br>\\( p = \\frac{-8}{4} = -2 \\).<br>Oś symetrii: \\( x = -2 \\).'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Wyznacz punkt przecięcia paraboli \\( y = -3x^2 + 2x - 5 \\) z osią OY.',
+        rozwiazanie: 'Punkt na osi OY ma pierwszą współrzędną 0. Podstawiamy x=0 do wzoru funkcji.<br>\\( f(0) = -5 \\).<br>Punkt to (0, -5). Oznacza to, że punkt ten wskazuje wyraz wolny równania c.'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Jaka jest najmniejsza wartość funkcji \\( f(x) = x^2 + 2x \\)?',
+        rozwiazanie: 'Ramiona paraboli są w górę (a=1), więc najmniejsza wartość to współrzędna q wierzchołka.<br>\\( p = \\frac{-2}{2} = -1 \\).<br>\\( q = f(-1) = (-1)^2 + 2(-1) = 1 - 2 = -1 \\).<br>Najmniejsza wartość to -1.'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Zapisz funkcję \\( y = (x - 1)^2 - 4 \\) w postaci ogólnej.',
+        rozwiazanie: 'Podnosimy nawias do kwadratu ze wzorów skróconego mnożenia i redukujemy wyrazy:<br>\\( x^2 - 2x + 1 - 4 = x^2 - 2x - 3 \\).<br>To jest postać ogólna.'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Rozwiąż równanie \\( f(x) = 0 \\) dla funkcji danej wzorem \\( f(x) = 2(x - 3)(x + 4) \\).',
+        rozwiazanie: 'Równanie ma postać iloczynową. Iloczyn jest równy 0, gdy dowolny z nawiasów jest równy 0.<br>\\( x - 3 = 0 \\Rightarrow x = 3 \\)<br>\\( x + 4 = 0 \\Rightarrow x = -4 \\)'
+    },
+    {
+        poddzial: 'Funkcja kwadratowa',
+        tresc: 'Oblicz maksymalną wartość funkcji \\( y = -x^2 + 6x \\).',
+        rozwiazanie: 'Ramiona w dół (a=-1), szukamy współrzędnej q wierzchołka.<br>\\( p = \\frac{-6}{-2} = 3 \\).<br>\\( q = f(3) = -(3^2) + 6 \\cdot 3 = -9 + 18 = 9 \\).<br>Maksymalna wartość to 9.'
+    }
+];
