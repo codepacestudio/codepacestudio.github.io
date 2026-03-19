92259a7800b339c5dc4d8cc30251b1a6ea26b61c
codepacestudio <codepacestudio@gmail.com>
2026-03-20 00:43:23 +0100

    Update rownania.js

--- matura/rownania.js
+++ matura/rownania.js
@@ -1,1 +1,152 @@
-
+bazaZadan['rownania'] = [
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( 2x - 5 = 3(x - 1) \\)',
+        rozwiazanie: 'Wymnażamy nawias:<br>\\( 2x - 5 = 3x - 3 \\)<br>Przenosimy x na lewo, liczby na prawo:<br>\\( 2x - 3x = -3 + 5 \\)<br>\\( -x = 2 \\)<br>\\( x = -2 \\)'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( \\frac{x}{2} + \\frac{x}{3} = 5 \\)',
+        rozwiazanie: 'Mnożymy obustronnie przez wspólny mianownik (6):<br>\\( 3x + 2x = 30 \\)<br>\\( 5x = 30 \\)<br>\\( x = 6 \\)'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie kwadratowe: \\( x^2 - 5x + 6 = 0 \\)',
+        rozwiazanie: 'Liczymy deltę:<br>\\( \\Delta = (-5)^2 - 4 \\cdot 1 \\cdot 6 = 25 - 24 = 1 \\)<br>\\( \\sqrt{\\Delta} = 1 \\)<br>\\( x_1 = \\frac{5 - 1}{2} = 2 \\)<br>\\( x_2 = \\frac{5 + 1}{2} = 3 \\)'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( x^2 - 4x = 0 \\)',
+        rozwiazanie: 'Wyłączamy x przed nawias:<br>\\( x(x - 4) = 0 \\)<br>Iloczyn jest równy zero, gdy jeden z czynników to zero:<br>\\( x = 0 \\) lub \\( x - 4 = 0 \\)<br>Odpowiedź: \\( x = 0 \\) lub \\( x = 4 \\)'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( 2x^2 - 18 = 0 \\)',
+        rozwiazanie: 'Dzielimy obustronnie przez 2:<br>\\( x^2 - 9 = 0 \\)<br>Korzystamy ze wzoru skróconego mnożenia:<br>\\( (x - 3)(x + 3) = 0 \\)<br>\\( x = 3 \\) lub \\( x = -3 \\)'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( x^2 + 6x + 9 = 0 \\)',
+        rozwiazanie: 'Zauważamy wzór skróconego mnożenia:<br>\\( (x + 3)^2 = 0 \\)<br>Równanie ma jedno rozwiązanie podwójne:<br>\\( x = -3 \\)'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( x^2 + 4 = 0 \\)',
+        rozwiazanie: 'Przekształcamy:<br>\\( x^2 = -4 \\)<br>Kwadrat liczby rzeczywistej nie może być ujemny. Równanie sprzeczne, brak rozwiązań.'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( (x - 1)(x + 2)(x - 3) = 0 \\)',
+        rozwiazanie: 'Równanie jest w postaci iloczynowej. Przyrównujemy każdy nawias do zera:<br>\\( x - 1 = 0 \\Rightarrow x = 1 \\)<br>\\( x + 2 = 0 \\Rightarrow x = -2 \\)<br>\\( x - 3 = 0 \\Rightarrow x = 3 \\)'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie: \\( \\frac{2}{x - 1} = \\frac{3}{x + 2} \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq 1 \\) i \\( x \\neq -2 \\).<br>Mnożymy na krzyż:<br>\\( 2(x + 2) = 3(x - 1) \\)<br>\\( 2x + 4 = 3x - 3 \\)<br>\\( -x = -7 \\)<br>\\( x = 7 \\) (należy do dziedziny).'
+    },
+    {
+        poddzial: 'Równania liniowe i kwadratowe',
+        tresc: 'Rozwiąż równanie wielomianowe: \\( x^3 - 4x = 0 \\)',
+        rozwiazanie: 'Wyłączamy x przed nawias:<br>\\( x(x^2 - 4) = 0 \\)<br>Rozkładamy nawias ze wzoru skróconego mnożenia:<br>\\( x(x - 2)(x + 2) = 0 \\)<br>Rozwiązania: \\( x = 0, x = 2, x = -2 \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność: \\( 3x - 4 < 2x + 1 \\)',
+        rozwiazanie: 'Przenosimy wiadome i niewiadome:<br>\\( 3x - 2x < 1 + 4 \\)<br>\\( x < 5 \\)<br>Przedział: \\( x \\in (-\\infty, 5) \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność: \\( -2x + 6 \\ge 10 \\)',
+        rozwiazanie: 'Przenosimy 6 na prawą stronę:<br>\\( -2x \\ge 4 \\)<br>Dzielimy przez -2, pamiętając o odwróceniu znaku nierówności!<br>\\( x \\le -2 \\)<br>Przedział: \\( x \\in (-\\infty, -2] \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż podwójną nierówność: \\( -2 < 3x + 1 \\le 10 \\)',
+        rozwiazanie: 'Odejmujemy 1 od wszystkich stron:<br>\\( -3 < 3x \\le 9 \\)<br>Dzielimy przez 3:<br>\\( -1 < x \\le 3 \\)<br>Przedział: \\( x \\in (-1, 3] \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność kwadratową: \\( x^2 - 4x + 3 > 0 \\)',
+        rozwiazanie: 'Liczymy pierwiastki:\\( \\Delta = 16 - 12 = 4 \\), \\( x_1 = 1 \\), \\( x_2 = 3 \\).<br>Ramiona paraboli skierowane w górę. Wartości dodatnie są na zewnątrz pierwiastków.<br>Przedział: \\( x \\in (-\\infty, 1) \\cup (3, \\infty) \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność: \\( -x^2 + 4 \\ge 0 \\)',
+        rozwiazanie: 'Mnożymy przez -1 i odwracamy znak:<br>\\( x^2 - 4 \\le 0 \\)<br>Pierwiastki to -2 i 2. Ramiona w górę, szukamy wartości ujemnych (pod osią).<br>Przedział: \\( x \\in [-2, 2] \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność: \\( x^2 + x + 1 > 0 \\)',
+        rozwiazanie: 'Liczymy deltę:<br>\\( \\Delta = 1 - 4 = -3 \\)<br>Delta ujemna, brak miejsc zerowych. Ramiona w górę (a > 0), więc cała parabola leży nad osią OX.<br>Nierówność jest spełniona dla każdego x. \\( x \\in \\mathbb{R} \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność: \\( x^2 - 2x + 1 \\le 0 \\)',
+        rozwiazanie: 'Zwijamy ze wzoru skróconego mnożenia:<br>\\( (x - 1)^2 \\le 0 \\)<br>Kwadrat nie może być ujemny, może być tylko równy zero.<br>Rozwiązanie to jeden punkt: \\( x = 1 \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność: \\( \\frac{x - 1}{2} - \\frac{x}{3} > 1 \\)',
+        rozwiazanie: 'Mnożymy przez wspólny mianownik (6):<br>\\( 3(x - 1) - 2x > 6 \\)<br>\\( 3x - 3 - 2x > 6 \\)<br>\\( x > 9 \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Znajdź część wspólną przedziałów: \\( x > 2 \\) i \\( x \\le 5 \\)',
+        rozwiazanie: 'Rysujemy oś liczbową. Szukamy liczb jednocześnie większych od 2 i mniejszych lub równych 5.<br>Rozwiązanie: \\( x \\in (2, 5] \\)'
+    },
+    {
+        poddzial: 'Nierówności',
+        tresc: 'Rozwiąż nierówność z wartością bezwzględną: \\( |x - 2| < 3 \\)',
+        rozwiazanie: 'Rozpisujemy na podwójną nierówność:<br>\\( -3 < x - 2 < 3 \\)<br>Dodajemy 2 do wszystkich stron:<br>\\( -1 < x < 5 \\)<br>Przedział: \\( x \\in (-1, 5) \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{x - 3}{x + 2} = 0 \\)',
+        rozwiazanie: 'Wyznaczamy dziedzinę: mianownik \\( x + 2 \\neq 0 \\Rightarrow x \\neq -2 \\).<br>Ułamek jest równy zero, gdy jego licznik jest równy zero:<br>\\( x - 3 = 0 \\Rightarrow x = 3 \\). Liczba należy do dziedziny.'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{2x}{x - 1} = 3 \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq 1 \\).<br>Mnożymy obustronnie przez mianownik:<br>\\( 2x = 3(x - 1) \\)<br>\\( 2x = 3x - 3 \\)<br>\\( -x = -3 \\Rightarrow x = 3 \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{x - 1}{x} = \\frac{x}{x + 3} \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq 0 \\) i \\( x \\neq -3 \\).<br>Mnożymy na krzyż:<br>\\( (x - 1)(x + 3) = x \\cdot x \\)<br>\\( x^2 + 3x - x - 3 = x^2 \\)<br>\\( 2x = 3 \\Rightarrow x = 1.5 \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{x^2 - 9}{x - 3} = 0 \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq 3 \\).<br>Przyrównujemy licznik do zera:<br>\\( x^2 - 9 = 0 \\Rightarrow x = 3 \\) lub \\( x = -3 \\).<br>Wynik \\( x = 3 \\) odrzucamy (poza dziedziną).<br>Jedyna odpowiedź: \\( x = -3 \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{1}{x} + \\frac{2}{x} = 3 \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq 0 \\).<br>Mianowniki są równe, dodajemy liczniki:<br>\\( \\frac{3}{x} = 3 \\)<br>Mnożymy przez x:<br>\\( 3 = 3x \\Rightarrow x = 1 \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{2}{x + 1} - \\frac{1}{x - 1} = 0 \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq -1 \\) i \\( x \\neq 1 \\).<br>Przenosimy jeden ułamek na prawą stronę:<br>\\( \\frac{2}{x + 1} = \\frac{1}{x - 1} \\)<br>Mnożymy na krzyż:<br>\\( 2(x - 1) = x + 1 \\)<br>\\( 2x - 2 = x + 1 \\Rightarrow x = 3 \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{(x - 1)(x + 4)}{x^2 - 1} = 0 \\)',
+        rozwiazanie: 'Dziedzina: \\( x^2 - 1 \\neq 0 \\Rightarrow x \\neq 1 \\) i \\( x \\neq -1 \\).<br>Przyrównujemy licznik do zera:<br>\\( x = 1 \\) (odrzucamy) lub \\( x = -4 \\).<br>Odpowiedź: \\( x = -4 \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{x^2 - 4x + 4}{x - 2} = 0 \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq 2 \\).<br>Licznik zwijamy ze wzoru: \\( (x - 2)^2 = 0 \\Rightarrow x = 2 \\).<br>Rozwiązanie wyklucza się z dziedziną. Równanie sprzeczne.'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{x}{2} = \\frac{8}{x} \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq 0 \\).<br>Mnożymy na krzyż:<br>\\( x^2 = 16 \\)<br>Rozwiązania: \\( x = 4 \\) lub \\( x = -4 \\)'
+    },
+    {
+        poddzial: 'Równania wymierne',
+        tresc: 'Rozwiąż równanie: \\( \\frac{x - 2}{x + 2} = \\frac{1}{2} \\)',
+        rozwiazanie: 'Dziedzina: \\( x \\neq -2 \\).<br>Mnożymy na krzyż:<br>\\( 2(x - 2) = 1(x + 2) \\)<br>\\( 2x - 4 = x + 2 \\)<br>\\( x = 6 \\)'
+    }
+];
